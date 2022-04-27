8b31ba3297c4d4993a4e7cfc28de76b8fd545831
Juan Rodriguez Hortala <juanrh@redhat.com>
2022-04-27 14:43:02 +0200

    Account no wiring for workers in TestInjections

--- cmd/fleet-manager/main_test.go
+++ cmd/fleet-manager/main_test.go
@@ -3,18 +3,18 @@ package main
 import (
 	"testing"
 
-	. "github.com/onsi/gomega"
-	"github.com/stackrox/acs-fleet-manager/internal/dinosaur"
+	"github.com/stackrox/acs-fleet-manager/internal/rhacs"
 	"github.com/stackrox/acs-fleet-manager/pkg/environments"
 	"github.com/stackrox/acs-fleet-manager/pkg/server"
-	"github.com/stackrox/acs-fleet-manager/pkg/workers"
+	// "github.com/stackrox/acs-fleet-manager/pkg/workers"
+	. "github.com/onsi/gomega"
 )
 
 func TestInjections(t *testing.T) {
 	RegisterTestingT(t)
 
 	env, err := environments.New(environments.DevelopmentEnv,
-		dinosaur.ConfigProviders(),
+		rhacs.ConfigProviders(),
 	)
 	Expect(err).To(BeNil())
 	err = env.CreateServices()
@@ -22,7 +22,7 @@ func TestInjections(t *testing.T) {
 
 	var bootList []environments.BootService
 	env.MustResolve(&bootList)
-	Expect(len(bootList)).To(Equal(4))
+	Expect(len(bootList)).To(Equal(3)) // FIXME restore when workers are enable again: Expect(len(bootList)).To(Equal(4))
 
 	_, ok := bootList[0].(*server.ApiServer)
 	Expect(ok).To(Equal(true))
@@ -30,11 +30,13 @@ func TestInjections(t *testing.T) {
 	Expect(ok).To(Equal(true))
 	_, ok = bootList[2].(*server.HealthCheckServer)
 	Expect(ok).To(Equal(true))
+	/* FIXME restore when workers are enable again: Expect(len(bootList)).To(Equal(4))
 	_, ok = bootList[3].(*workers.LeaderElectionManager)
 	Expect(ok).To(Equal(true))
+	
 
 	var workerList []workers.Worker
 	env.MustResolve(&workerList)
 	Expect(workerList).To(HaveLen(8))
-
+	*/
 }
