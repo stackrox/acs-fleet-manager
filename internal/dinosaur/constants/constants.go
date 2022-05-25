58d4756dae58a0fbcc74ab0023ad9466cccfcb6d
Simon Baeumer <sbaumer@redhat.com>
2022-05-25 10:18:52 +0200

    WIP

--- internal/dinosaur/constants/constants.go
+++ internal/dinosaur/constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	// ProductName defines the name of the product
-	ProductName = "Managed RHACS"
+	ProductName = "RHACS"
 	// DefaultClusterNodeScaleIncrement - default increment/ decrement node count when scaling multiAZ clusters
 	DefaultClusterNodeScaleIncrement = 3
 )
