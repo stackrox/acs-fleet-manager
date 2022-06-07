6edb031119215895f7ba77bd9386480ee4f4ffb3
Moritz Clasmeier <moritz@stackrox.com>
2022-06-07 15:24:00 +0200

    Shrink docker build context

---
+++ dev/env/patch-down-resources.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+kubectl -n test-central-1 patch deploy/central --patch '{"spec":{"template":{"spec":{"containers":[{"name":"central","resources":{"limits":{"cpu":"1","memory":"4Gi"},"requests":{"cpu":"1","memory":"1Gi"}}}]}}}}'
--- dev/env/watch-central-request.sh
+++ dev/env/watch-central-request.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-watch "psql -h localhost -d $POSTGRES_DB -U $POSTGRES_USER -c 'select name,status from dinosaur_requests;'"
-
+watch "psql -h localhost -d $POSTGRES_DB -U $POSTGRES_USER -c 'select * from dinosaur_requests;'"
