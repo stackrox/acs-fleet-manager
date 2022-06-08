67da38dd3ff8793f68dce9c534b3a3b776495683
Simon Baeumer <sbaumer@redhat.com>
2022-06-08 09:52:11 +0200

    Skip e2e tests by default

--- e2e/README.md
+++ e2e/README.md
@@ -13,7 +13,7 @@ $ make fleetshard-sync
 $ CLUSTER_ID=1234567890abcdef1234567890abcdef OCM_TOKEN=$(ocm token) ./fleetshard-sync
 
 # Run e2e tests
-$ OCM_TOKEN=$(ocm token) go test ./e2e/...
+$ RUN_E2E=true OCM_TOKEN=$(ocm token) go test ./e2e/...
 
 # To clean up the environment run
 $ ./e2e/cleanup.sh
--- e2e/e2e_suite_test.go
+++ e2e/e2e_suite_test.go
@@ -5,6 +5,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/stackrox/acs-fleet-manager/fleetshard/pkg/k8s"
 	"k8s.io/client-go/rest"
+	"os"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 )
@@ -13,6 +14,9 @@ var cfg *rest.Config
 var k8sClient client.Client
 
 func TestE2E(t *testing.T) {
+	if os.Getenv("RUN_E2E") != "true" {
+		t.Skip("Skip e2e tests. Set RUN_E2E=1 env variable to enable e2e tests.")
+	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "RHACS ManagedServices Suite")
 }
