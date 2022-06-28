c255f15974b885a0bf26dd15e565d2ef57f1e467
Daniel Haus <dhaus@redhat.com>
2022-06-28 13:27:55 +0200

    Add static token authentication within fleetshard

--- internal/dinosaur/pkg/environments/development.go
+++ internal/dinosaur/pkg/environments/development.go
@@ -32,5 +32,6 @@ func NewDevelopmentEnvLoader() environments.EnvLoader {
 		"observability-red-hat-sso-token-refresher-url":   "http://localhost:8085",
 		"observability-red-hat-sso-observatorium-gateway": "https://observatorium-mst.api.stage.openshift.com",
 		"observability-red-hat-sso-tenant":                "manageddinosaur",
+		"jwks-file":                                       "config/jwks-file-static.json",
 	}
 }
