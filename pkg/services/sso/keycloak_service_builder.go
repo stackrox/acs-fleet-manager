eb4f0a3f414459e9a4a0dcae62d24b75be271ff2
Daniel Haus <dhaus@redhat.com>
2022-07-04 05:04:14 +0200

    Remove OSDKeycloakService

--- pkg/services/sso/keycloak_service_builder.go
+++ pkg/services/sso/keycloak_service_builder.go
@@ -9,10 +9,8 @@ import (
 var _ KeycloakServiceBuilderSelector = &keycloakServiceBuilderSelector{}
 var _ KeycloakServiceBuilder = &keycloakServiceBuilder{}
 var _ ACSKeycloakServiceBuilderConfigurator = &keycloakBuilderConfigurator{}
-var _ OSDKeycloakServiceBuilderConfigurator = &osdBuilderConfigurator{}
 
 type KeycloakServiceBuilderSelector interface {
-	ForOSD() OSDKeycloakServiceBuilderConfigurator
 	ForACS() ACSKeycloakServiceBuilderConfigurator
 }
 
@@ -20,25 +18,12 @@ type ACSKeycloakServiceBuilderConfigurator interface {
 	WithConfiguration(config *iam.IAMConfig) KeycloakServiceBuilder
 }
 
-type OSDKeycloakServiceBuilderConfigurator interface {
-	WithConfiguration(config *iam.IAMConfig) OSDKeycloakServiceBuilder
-}
-
 type KeycloakServiceBuilder interface {
 	WithRealmConfig(realmConfig *iam.IAMRealmConfig) KeycloakServiceBuilder
 	Build() IAMService
 }
 
-type OSDKeycloakServiceBuilder interface {
-	WithRealmConfig(realmConfig *iam.IAMRealmConfig) OSDKeycloakServiceBuilder
-	Build() OSDKeycloakService
-}
-
 type keycloakServiceBuilderSelector struct {
-}
-
-func (s *keycloakServiceBuilderSelector) ForOSD() OSDKeycloakServiceBuilderConfigurator {
-	return &osdBuilderConfigurator{}
 }
 
 func (s *keycloakServiceBuilderSelector) ForACS() ACSKeycloakServiceBuilderConfigurator {
@@ -54,18 +39,10 @@ func (k *keycloakBuilderConfigurator) WithConfiguration(config *iam.IAMConfig) K
 	}
 }
 
-func (o *osdBuilderConfigurator) WithConfiguration(config *iam.IAMConfig) OSDKeycloakServiceBuilder {
-	return &osdKeycloackServiceBuilder{
-		config: config,
-	}
-}
-
 type keycloakServiceBuilder struct {
 	config      *iam.IAMConfig
 	realmConfig *iam.IAMRealmConfig
 }
-
-type osdKeycloackServiceBuilder keycloakServiceBuilder
 
 // Build returns an instance of IAMService ready to be used.
 // If a custom realm is configured (WithRealmConfig called), then always Keycloak provider is used
@@ -75,18 +52,6 @@ func (builder *keycloakServiceBuilder) Build() IAMService {
 }
 
 func (builder *keycloakServiceBuilder) WithRealmConfig(realmConfig *iam.IAMRealmConfig) KeycloakServiceBuilder {
-	builder.realmConfig = realmConfig
-	return builder
-}
-
-// Build returns an instance of IAMService ready to be used.
-// If a custom realm is configured (WithRealmConfig called), then always Keycloak provider is used
-// irrespective of the `builder.config.SelectSSOProvider` value
-func (builder *osdKeycloackServiceBuilder) Build() OSDKeycloakService {
-	return build(builder.config, builder.realmConfig).(OSDKeycloakService)
-}
-
-func (builder *osdKeycloackServiceBuilder) WithRealmConfig(realmConfig *iam.IAMRealmConfig) OSDKeycloakServiceBuilder {
 	builder.realmConfig = realmConfig
 	return builder
 }
