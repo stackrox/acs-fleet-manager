ef88719969c20750cf461672c06f0cf54f62045a
Moritz Clasmeier <moritz@stackrox.com>
2022-07-05 16:30:12 +0200

    Use oc in OpenShift CI

--- .openshift-ci/image-push/entrypoint.sh
+++ .openshift-ci/image-push/entrypoint.sh
@@ -17,22 +17,47 @@ if [[ -z "$FLEET_MANAGER_IMAGE" ]]; then
 fi
 
 registry_host=$(echo "$IMAGE_PUSH_REGISTRY" | cut -d / -f 1)
+tag=$(make -C "$GITROOT" tag)
+if [[ "$tag" =~ dirty$ ]]; then
+    die "Error: Repository is dirty, refusing to push dirty tag to registry."
+fi
+image_tag="${IMAGE_PUSH_REGISTRY}/acsms-test:${tag}"
 
-docker_logged_in() {
-    local host="$1"
-    local cfg=""
-    local docker_cfg="$HOME/.docker/config.json"
-    if [[ -f "$docker_cfg" ]]; then
-        cfg=$(cat "$docker_cfg")
-    fi
-    if echo "$cfg" | jq -r ".auths | keys[]" | grep -q "^${host}$"; then
-        return 0
-    else
-        return 1
-    fi
-}
+if [[ -z "$OPENSHIFT_CI" ]]; then
+    docker_logged_in() {
+        local host="$1"
+        local cfg=""
+        local docker_cfg="$HOME/.docker/config.json"
+        if [[ -f "$docker_cfg" ]]; then
+            cfg=$(cat "$docker_cfg")
+        fi
+        if echo "$cfg" | jq -r ".auths | keys[]" | grep -q "^${host}$"; then
+            return 0
+        else
+            return 1
+        fi
+    }
 
-if ! docker_logged_in "$registry_host"; then
+    if ! docker_logged_in "$registry_host"; then
+        if [[ -z "$IMAGE_PUSH_USERNAME" ]]; then
+            die "Error: IMAGE_PUSH_USERNAME not found. Either set it in the environment or make sure that the CI Vault contains this data."
+        fi
+
+        if [[ -z "$IMAGE_PUSH_PASSWORD" ]]; then
+            die "Error: IMAGE_PUSH_PASSWORD not found. Either set it in the environment or make sure that the CI Vault contains this data."
+        fi
+
+        log "Logging into ${registry_host}."
+        docker login -u "$IMAGE_PUSH_USERNAME" --password-stdin "$registry_host" <<<"$IMAGE_PUSH_PASSWORD"
+    fi
+
+    log "Creating tag ${image_tag}"
+    docker tag "$FLEET_MANAGER_IMAGE" "$image_tag"
+
+    log "Pushing tag ${image_tag}"
+    docker push "$image_tag"
+
+else
     if [[ -z "$IMAGE_PUSH_USERNAME" ]]; then
         die "Error: IMAGE_PUSH_USERNAME not found. Either set it in the environment or make sure that the CI Vault contains this data."
     fi
@@ -40,20 +65,6 @@ if ! docker_logged_in "$registry_host"; then
     if [[ -z "$IMAGE_PUSH_PASSWORD" ]]; then
         die "Error: IMAGE_PUSH_PASSWORD not found. Either set it in the environment or make sure that the CI Vault contains this data."
     fi
-
-    log "Logging into ${registry_host}."
-    docker login -u "$IMAGE_PUSH_USERNAME" --password-stdin "$registry_host" <<<"$IMAGE_PUSH_PASSWORD"
+    oc registry login --auth-basic="${IMAGE_PUSH_USERNAME}:${IMAGE_PUSH_PASSWORD}" --registry="$registry_host"
+    oc image mirror "$FLEET_MANAGER_IMAGE" "$image_tag"
 fi
-
-tag=$(make -C "$GITROOT" tag)
-if [[ "$tag" =~ dirty$ ]]; then
-    die "Error: Repository is dirty, refusing to push dirty tag to registry."
-fi
-
-image_tag="${IMAGE_PUSH_REGISTRY}/acs-fleet-manager-image:${tag}"
-
-log "Creating tag ${image_tag}"
-docker tag "$FLEET_MANAGER_IMAGE" "$image_tag"
-
-log "Pushing tag ${image_tag}"
-docker push "$image_tag"
