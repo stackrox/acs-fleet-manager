b075defb67916b1b74edd12f0bfcfa63bf17ee7f
Moritz Clasmeier <moritz@stackrox.com>
2022-07-10 01:52:11 +0200

    Polishing of the test environment, reducing complexity, streamlining configuration of different cluster profiles, restructuring.

--- dev/env/scripts/up.sh
+++ dev/env/scripts/up.sh
@@ -18,15 +18,21 @@ Installing RHACS Operator: ${INSTALL_OPERATOR}
 
 EOF
 
-if [[ -z "$OPENSHIFT_CI" ]]; then
-    if [[ "$FLEET_MANAGER_IMAGE" =~ ^fleet-manager-.*/fleet-manager:.* ]]; then
+disable_debugging
+KUBE_CONFIG=$(assemble_kubeconfig | yq e . -j - | jq -c . -)
+enable_debugging_if_necessary
+export KUBE_CONFIG
+
+if [[ "$CLUSTER_TYPE" != "openshift-ci" ]]; then
+    if [[ "$FLEET_MANAGER_IMAGE" =~ ^fleet-manager:.* ]]; then
         # Local image reference, which cannot be pulled.
-        if ! $DOCKER image inspect "${FLEET_MANAGER_IMAGE}" >/dev/null 2>&1; then
+        image_available=$(if $DOCKER image inspect "${FLEET_MANAGER_IMAGE}" >/dev/null 2>&1; then echo "true"; else echo "false"; fi)
+        if [[ "$image_available" != "true" || "$FLEET_MANAGER_IMAGE" =~ dirty$ ]]; then
             # Attempt to build this image.
-            if [[ "$FLEET_MANAGER_IMAGE" == "$(make -s -C "${GITROOT}" image-tag)" ]]; then
+            if [[ "$FLEET_MANAGER_IMAGE" == "fleet-manager:$(make -s -C "${GITROOT}" tag)" ]]; then
                 # Looks like we can build this tag from the current state of the repository.
                 log "Rebuilding image..."
-                make -C "${GITROOT}" image/build
+                make -C "${GITROOT}" image/build/local
             else
                 die "Cannot find image '${FLEET_MANAGER_IMAGE}' and don't know how to build it"
             fi
@@ -37,48 +43,40 @@ if [[ -z "$OPENSHIFT_CI" ]]; then
     fi
 fi
 
-if [[ "$CLUSTER_TYPE" == "minikube" ]]; then
-    # Workaround for a bug in minikube(?) where sometimes the images fail to load:
-    log "Deleting docker containers running in Minikube"
-    $MINIKUBE ssh 'docker kill $(docker ps -q) > /dev/null' || true
-    sleep 1
-    $MINIKUBE ssh 'docker rm --force $(docker ps -a -q) > /dev/null' || true
-    sleep 1
-    $MINIKUBE image ls | grep -v "^${FLEET_MANAGER_IMAGE}$" | { grep "^.*/fleet-manager-.*/fleet-manager:.*$" || test $? = 1; } | while read -r img; do
-        $MINIKUBE image rm "$img" || true
-    done
-    # In a perfect world this line would be sufficient...
-    $DOCKER save "${FLEET_MANAGER_IMAGE}" | $MINIKUBE ssh --native-ssh=false docker load
-    $MINIKUBE image ls | grep -q "^${FULL_FLEET_MANAGER_IMAGE}$" || {
-        # Double check the image is there -- has been failing often enough due to the bug with the above workaround.
-        die "Image ${FULL_FLEET_MANAGER_IMAGE} not available in cluster, aborting"
-    }
+# Verify that the image is there.
+if ! $DOCKER image inspect "$FLEET_MANAGER_IMAGE" >/dev/null 2>&1; then
+    die "Image ${FLEET_MANAGER_IMAGE} not available in cluster, aborting"
 fi
 
-# Apply cluster type specific manifests.
+# Apply cluster type specific manifests, if any.
 if [[ -d "${MANIFESTS_DIR}/cluster-type-${CLUSTER_TYPE}" ]]; then
     apply "${MANIFESTS_DIR}/cluster-type-${CLUSTER_TYPE}"
 fi
 
 # Deploy database.
+log "Deploying database"
 apply "${MANIFESTS_DIR}/db"
 wait_for_container_to_become_ready "$ACSMS_NAMESPACE" "application=db" "db"
 log "Database is ready."
 
 # Deploy MS components.
+log "Deploying fleet-manager"
 apply "${MANIFESTS_DIR}/fleet-manager"
 wait_for_container_to_appear "$ACSMS_NAMESPACE" "application=fleet-manager" "db-migrate"
 wait_for_container_to_appear "$ACSMS_NAMESPACE" "application=fleet-manager" "fleet-manager"
-if [[ "$SPAWN_LOGGER" == "true" ]]; then
+if [[ "$SPAWN_LOGGER" == "true" && -n "${LOG_DIR:-}" ]]; then
     $KUBECTL -n "$ACSMS_NAMESPACE" logs -l application=fleet-manager --all-containers --pod-running-timeout=1m --since=1m --tail=100 -f >"${LOG_DIR}/pod-logs_fleet-manager.txt" 2>&1 &
 fi
 
+log "Deploying fleetshard-sync"
 apply "${MANIFESTS_DIR}/fleetshard-sync"
 wait_for_container_to_appear "$ACSMS_NAMESPACE" "application=fleetshard-sync" "fleetshard-sync"
-if [[ "$SPAWN_LOGGER" == "true" ]]; then
+if [[ "$SPAWN_LOGGER" == "true" && -n "${LOG_DIR:-}" ]]; then
     $KUBECTL -n "$ACSMS_NAMESPACE" logs -l application=fleetshard-sync --all-containers --pod-running-timeout=1m --since=1m --tail=100 -f >"${LOG_DIR}/pod-logs_fleetshard-sync_fleetshard-sync.txt" 2>&1 &
 fi
 
+# Sanity check.
+wait_for_container_to_become_ready "$ACSMS_NAMESPACE" "application=fleetshard-sync" "fleetshard-sync"
 # Prerequisite for port-forwarding are pods in ready state.
 wait_for_container_to_become_ready "$ACSMS_NAMESPACE" "application=fleet-manager" "fleet-manager"
 
