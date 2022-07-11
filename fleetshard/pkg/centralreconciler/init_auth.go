20fcbbe297de9c3e31e186e27e4c37c85eb18d87
ivan-degtiarenko <degtiarenko@stackrox.com>
2022-07-12 00:04:17 +0200

    Add groups to auth provider; address comments

--- fleetshard/pkg/centralreconciler/init_auth.go
+++ fleetshard/pkg/centralreconciler/init_auth.go
@@ -1,17 +1,17 @@
 package centralreconciler
 
 import (
-	"bytes"
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"github.com/golang/glog"
 	"github.com/pkg/errors"
+	centralClient "github.com/stackrox/acs-fleet-manager/fleetshard/pkg/central_client"
 	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/api/private"
 	acsErrors "github.com/stackrox/acs-fleet-manager/pkg/errors"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/httputil"
 	core "k8s.io/api/core/v1"
-	"net/http"
 	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -22,54 +22,96 @@ const (
 )
 
 var (
-	insecureTransport *http.Transport
+	groupCreators = []func(providerId string, auth private.ManagedCentralAllOfSpecAuth) *storage.Group{
+		func(providerId string, auth private.ManagedCentralAllOfSpecAuth) *storage.Group {
+			return &storage.Group{
+				Props: &storage.GroupProperties{
+					AuthProviderId: providerId,
+				},
+				RoleName: "None",
+			}
+		},
+		func(providerId string, auth private.ManagedCentralAllOfSpecAuth) *storage.Group {
+			return &storage.Group{
+				Props: &storage.GroupProperties{
+					AuthProviderId: providerId,
+					Key:            "userid",
+					Value:          auth.OwnerUserId,
+				},
+				RoleName: "Admin",
+			}
+		},
+		func(providerId string, auth private.ManagedCentralAllOfSpecAuth) *storage.Group {
+			return &storage.Group{
+				Props: &storage.GroupProperties{
+					AuthProviderId: providerId,
+					Key:            "groups",
+					Value:          "org_admin",
+				},
+				RoleName: "Admin",
+			}
+		},
+	}
 )
 
-func init() {
-	insecureTransport = http.DefaultTransport.(*http.Transport).Clone()
-	// TODO: once certificates will be added, we probably will be able to replace with secure transport
-	insecureTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+type AuthProviderResponse struct {
+	Id string `json:"id"`
 }
 
-// InitRHSSOAuthProvider initialises sso.redhat.com auth provider in a deployed Central instance.
-func InitRHSSOAuthProvider(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) error {
-	// Acquire central admin password.
-	pass, err := acquireAdminPassword(ctx, central, client)
+// CreateRHSSOAuthProvider initialises sso.redhat.com auth provider in a deployed Central instance.
+func CreateRHSSOAuthProvider(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) error {
+	pass, err := getAdminPassword(ctx, central, client)
 	if err != nil {
 		return err
 	}
 
-	// Acquire central address.
-	address, err := acquireServiceAddress(ctx, central, client)
+	address, err := getServiceAddress(ctx, central, client)
 	if err != nil {
 		return err
 	}
 
-	// Send POST request to Central.
+	// Send auth provider POST request to Central.
 	authProviderRequest := createAuthProviderRequest(central)
-	jsonBytes, err := json.Marshal(authProviderRequest)
+	resp, err := centralClient.SendRequestToCentral(ctx, authProviderRequest, address+"/v1/authProviders", pass)
 	if err != nil {
-		return errors.Wrap(err, "marshalling new auth provider request to central")
-	}
-	req, err := http.NewRequest(http.MethodPost, address, bytes.NewBuffer(jsonBytes))
-	if err != nil {
-		return errors.Wrap(err, "creating HTTP request to central")
-	}
-	req.SetBasicAuth("admin", pass)
-	req = req.WithContext(ctx)
-
-	httpClient := http.Client{
-		Transport: insecureTransport,
-	}
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return errors.Wrap(err, "sending new auth provider request to central")
-	}
-	if resp.StatusCode == http.StatusOK {
-		return nil
-	} else {
+		return errors.Wrap(err, "sending new auth provider to central")
+	} else if !httputil.Is2xxStatusCode(resp.StatusCode) {
 		return acsErrors.NewErrorFromHTTPStatusCode(resp.StatusCode, "Failed to create auth provider for central %s", central.Metadata.Name)
 	}
+
+	// Decode auth provider response to use auth provider id in /v1/groups requests.
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			glog.Warningf("Attempt to close auth provider response failed: %s", err)
+		}
+	}()
+	var authProviderResp AuthProviderResponse
+	err = json.NewDecoder(resp.Body).Decode(&authProviderResp)
+	if err != nil {
+		return errors.Wrap(err, "decoding auth provider POST response")
+	}
+
+	// Initiate sso.redhat.com auth provider groups.
+	for _, groupCreator := range groupCreators {
+		group := groupCreator(authProviderResp.Id, central.Spec.Auth)
+		err = sendGroupRequest(ctx, central, group, address, pass)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func sendGroupRequest(ctx context.Context, central private.ManagedCentral, groupRequest *storage.Group, address string, pass string) error {
+	resp, err := centralClient.SendRequestToCentral(ctx, groupRequest, address+"/v1/groups", pass)
+	if err != nil {
+		return errors.Wrap(err, "sending new group to central")
+	}
+	if !httputil.Is2xxStatusCode(resp.StatusCode) {
+		return acsErrors.NewErrorFromHTTPStatusCode(resp.StatusCode, "Failed to create group for central %s", central.Metadata.Name)
+	}
+	return nil
 }
 
 func createAuthProviderRequest(central private.ManagedCentral) *storage.AuthProvider {
@@ -93,22 +135,22 @@ func createAuthProviderRequest(central private.ManagedCentral) *storage.AuthProv
 }
 
 // TODO: ROX-11644: doesn't work when fleetshard-sync deployed outside of Central's cluster
-func acquireServiceAddress(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) (string, error) {
+func getServiceAddress(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) (string, error) {
 	service := &core.Service{}
 	err := client.Get(ctx,
 		ctrlClient.ObjectKey{Name: centralServiceName, Namespace: central.Metadata.Namespace},
 		service)
 	if err != nil {
-		return "", errors.Wrapf(err, "obtain k8s service for central")
+		return "", errors.Wrapf(err, "getting k8s service for central")
 	}
 	if len(service.Spec.Ports) == 0 {
 		return "", errors.Errorf("No ports present in %s service", centralServiceName)
 	}
-	address := fmt.Sprintf("https://%s.%s.svc.cluster.local:%d/v1/authProviders", centralServiceName, central.Metadata.Namespace, service.Spec.Ports[0].Port)
+	address := fmt.Sprintf("https://%s.%s.svc.cluster.local:%d", centralServiceName, central.Metadata.Namespace, service.Spec.Ports[0].Port)
 	return address, nil
 }
 
-func acquireAdminPassword(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) (string, error) {
+func getAdminPassword(ctx context.Context, central private.ManagedCentral, client ctrlClient.Client) (string, error) {
 	secretRef := ctrlClient.ObjectKey{
 		Name:      centralHtpasswdSecretName,
 		Namespace: central.Metadata.Namespace,
@@ -116,11 +158,11 @@ func acquireAdminPassword(ctx context.Context, central private.ManagedCentral, c
 	secret := &core.Secret{}
 	err := client.Get(ctx, secretRef, secret)
 	if err != nil {
-		return "", errors.Wrap(err, "obtaining admin password secret")
+		return "", errors.Wrap(err, "getting admin password secret")
 	}
 	password := string(secret.Data[adminPasswordSecretKey])
 	if password == "" {
-		return "", errors.Errorf("No password present in %s secret. This should not be the case in Central instances installed via operator.", centralHtpasswdSecretName)
+		return "", errors.Errorf("no password present in %s secret.", centralHtpasswdSecretName)
 	}
 	return password, nil
 }
--- fleetshard/pkg/centralreconciler/reconciler.go
+++ fleetshard/pkg/centralreconciler/reconciler.go
@@ -159,7 +159,7 @@ func (r *CentralReconciler) Reconcile(ctx context.Context, remoteCentral private
 	// 2. OR reconciler creator specified auth provider not to be created
 	// 3. OR Central request is in status "Ready" - meaning auth provider should've been initialised earlier
 	if r.createAuthProvider && remoteCentral.RequestStatus != centralConstants.DinosaurRequestStatusReady.String() {
-		err = InitRHSSOAuthProvider(ctx, remoteCentral, r.client)
+		err = CreateRHSSOAuthProvider(ctx, remoteCentral, r.client)
 		if err != nil {
 			return nil, err
 		} else {
