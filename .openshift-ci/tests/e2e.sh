b82e848b684b2f69b8c28af719dab65da6527157
Daniel Haus <dhaus@redhat.com>
2022-07-17 23:42:41 +0200

    Rebase e2e script, fix linting issues, ocm login with offline token, rename variable for OCM_TOKEN.

--- .openshift-ci/tests/e2e.sh
+++ .openshift-ci/tests/e2e.sh
@@ -25,6 +25,9 @@ if [[ "${OPENSHIFT_CI:-}" == "true" ]]; then
     export QUAY_TOKEN="${IMAGE_PUSH_PASSWORD:-}"
     export CLUSTER_TYPE="openshift-ci"
     export GOARGS="-mod=mod" # For some reason we need this in the offical base images.
+    # When running in OpenShift CI, ensure we also run the auth E2E tests.
+    RUN_AUTH_E2E="true"
+    export RUN_AUTH_E2E
 fi
 
 init
@@ -44,17 +47,6 @@ log "Cluster name: ${CLUSTER_NAME}"
 log "Image: ${FLEET_MANAGER_IMAGE}"
 log "Log directory: ${LOG_DIR:-(none)}"
 
-if [[ -n "$OPENSHIFT_CI" ]]; then
-    log "Test suite is running in OpenShift CI"
-    export GOARGS="-mod=mod" # For some reason we need this in the offical base images.
-
-    # When running in OpenShift CI, ensure we also run the auth E2E tests.
-    RUN_AUTH_E2E="true"
-    export RUN_AUTH_E2E
-fi
-
-disable_debugging
-
 # If auth E2E tests shall be run, ensure we have all authentication related secrets correctly set up.
 if [[ "$RUN_AUTH_E2E" == "true" ]]; then
     log "Setting up authentication related environment variables for auth E2E tests"
@@ -65,8 +57,9 @@ if [[ "$RUN_AUTH_E2E" == "true" ]]; then
     export STATIC_TOKEN=${STATIC_TOKEN:-$FLEET_STATIC_TOKEN}
 
     # Ensure we set the OCM refresh token once more, in case AUTH_TYPE!=OCM.
-    OCM_SERVICE_TOKEN=$(ocm token --refresh)
-    export OCM_SERVICE_TOKEN
+    ocm login --token ${OCM_OFFLINE_TOKEN}
+    OCM_TOKEN=$(ocm token --refresh)
+    export OCM_TOKEN
 
     # The RH SSO secrets are correctly set up within vault, the tests will be skipped if they are empty.
 fi
@@ -74,9 +67,10 @@ fi
 case "$AUTH_TYPE" in
 OCM)
 
-    log "Refreshing OCM Service Token"
-    OCM_SERVICE_TOKEN=$(ocm token --refresh)
-    export OCM_SERVICE_TOKEN
+    log "Logging in with client credentials + Refreshing OCM Service Token"
+    ocm login --token ${OCM_OFFLINE_TOKEN}
+    OCM_TOKEN=$(ocm token --refresh)
+    export OCM_TOKEN
     ;;
 
 STATIC_TOKEN)
