1d3c6c22691c921c79fadbac2f2591f9f8f4b2ed
Vlad Bologa <vbologa@redhat.com>
2022-07-26 10:36:26 +0200

    Apply suggestions from code review
    Co-authored-by: Moritz Clasmeier <mtesseract@users.noreply.github.com>

--- internal/dinosaur/pkg/clusters/standalone_provider.go
+++ internal/dinosaur/pkg/clusters/standalone_provider.go
@@ -471,7 +471,7 @@ func applyResource(dynamicClient dynamic.Interface, mapper *restmapper.DeferredD
 	err = json.Unmarshal(data, &obj)
 
 	if err != nil {
-		return nil, fmt.Errorf("unmarshalling data: %w", err)
+		return nil, fmt.Errorf("unmarshaling data from JSON: %w", err)
 	}
 
 	newConfiguration := string(data)
