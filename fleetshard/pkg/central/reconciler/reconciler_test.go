3f1e6f692c27ec844df92fae98fbeb6e34c8ef45
Yury Kovalev <ykovalev@redhat.com>
2022-07-29 16:20:57 +0200

    Change routes statues

--- fleetshard/pkg/central/reconciler/reconciler_test.go
+++ fleetshard/pkg/central/reconciler/reconciler_test.go
@@ -287,7 +287,7 @@ func TestReportRoutesStatuses(t *testing.T) {
 			Router: "router-default.apps.test.local",
 		},
 		{
-			Domain: "central.rhacs-cb45idheg5ip6dq1jo4g",
+			Domain: "acs-data-cb45idheg5ip6dq1jo4g.acs.rhcloud.test",
 			Router: "router-default.apps.test.local",
 		},
 	}
@@ -313,7 +313,7 @@ func TestReportRoutesStatusWhenCentralNotChanged(t *testing.T) {
 			Router: "router-default.apps.test.local",
 		},
 		{
-			Domain: "central.rhacs-cb45idheg5ip6dq1jo4g",
+			Domain: "acs-data-cb45idheg5ip6dq1jo4g.acs.rhcloud.test",
 			Router: "router-default.apps.test.local",
 		},
 	}
