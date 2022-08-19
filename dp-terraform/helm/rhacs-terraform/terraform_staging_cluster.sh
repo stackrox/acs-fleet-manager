cdc43dd72044b521309efcbb746c3a82c069407e
Stephan Heßelmann <shesselm@redhat.com>
2022-08-19 16:36:05 +0200

    Add observability variables

--- dp-terraform/helm/rhacs-terraform/terraform_staging_cluster.sh
+++ dp-terraform/helm/rhacs-terraform/terraform_staging_cluster.sh
@@ -35,4 +35,7 @@ helm install rhacs-terraform \
   --set fleetshardSync.clusterId=${CLUSTER_ID} \
   --set acsOperator.enabled=true . \
   --set acsOperator.source=rhacs-operators \
-  --set acsOperator.startingCSV=rhacs-operator.v3.71.0
+  --set acsOperator.startingCSV=rhacs-operator.v3.71.0 \
+  --set observability.enabled=true . \
+  --set observability.github.repository=https://api.github.com/repos/stackrox/rhacs-observability-resources/contents \
+  --set observability.gateway=https://observatorium-mst.api.stage.openshift.com
