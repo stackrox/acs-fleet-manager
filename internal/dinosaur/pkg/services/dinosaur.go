329d71a49f1bdc5b62d434d1533c2e89fbbe4116
Alex Rukletsov <alexr@stackrox.com>
2022-09-05 17:36:26 +0200

    Fix column name.

--- internal/dinosaur/pkg/services/dinosaur.go
+++ internal/dinosaur/pkg/services/dinosaur.go
@@ -849,7 +849,7 @@ func (k *dinosaurService) ListCentralsWithoutAuthConfig() ([]*dbapi.CentralReque
 
 	dbConn := k.connectionFactory.New().
 		Where("status IN (?)", status).
-		Where("idp_client_id == ''")
+		Where("client_id == ''")
 
 	var results []*dbapi.CentralRequest
 	if err := dbConn.Find(&results).Error; err != nil {
