b9177caa4ae8d304bc37af846d2db24527ab490c
Simon Baeumer <sbaumer@redhat.com>
2022-09-07 21:56:19 +0200

    Add deletion and creation runtime test

--- fleetshard/pkg/runtime/runtime_test.go
+++ fleetshard/pkg/runtime/runtime_test.go
@@ -1,25 +1,39 @@
 package runtime
 
 import (
+	"context"
 	"github.com/stackrox/acs-fleet-manager/fleetshard/config"
 	"github.com/stackrox/acs-fleet-manager/fleetshard/pkg/fleetmanager"
 	"github.com/stackrox/acs-fleet-manager/fleetshard/pkg/testutils"
-	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/api/public"
+	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/api/private"
+	"github.com/stackrox/rox/operator/apis/platform/v1alpha1"
 	"github.com/stretchr/testify/require"
+	apiErrors "k8s.io/apimachinery/pkg/api/errors"
+	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
+	"time"
 )
 
 func TestRuntime(t *testing.T) {
 	fakeClient := testutils.NewFakeClientBuilder(t).Build()
 	fleetManagerClientMock := &fleetmanager.FleetManagerClientMock{}
-	fleetManagerClientMock.CreateCentralFunc = func(request public.CentralRequestPayload) (*public.CentralRequest, error) {
-		return &public.CentralRequest{
-			Id:      "id",
-			Kind:    "",
-			MultiAz: true,
-			Region:  "us-east1",
-			Version: "3.70",
+	fleetManagerClientMock.GetManagedCentralListFunc = func() (*private.ManagedCentralList, error) {
+		return &private.ManagedCentralList{
+			Items: []private.ManagedCentral{
+				{
+					Id:   "id",
+					Kind: "",
+					Spec: private.ManagedCentralAllOfSpec{},
+					Metadata: private.ManagedCentralAllOfMetadata{
+						Name:      "test-central",
+						Namespace: "test-namespace",
+					},
+				},
+			},
 		}, nil
+	}
+	fleetManagerClientMock.UpdateStatusFunc = func(statuses map[string]private.DataPlaneCentralStatus) error {
+		return nil
 	}
 
 	runtime := Runtime{
@@ -27,8 +41,38 @@ func TestRuntime(t *testing.T) {
 		client:    fleetManagerClientMock,
 		config: &config.Config{
 			CreateAuthProvider: false,
+			RuntimePollPeriod:  1 * time.Second,
 		},
+		reconcilers: reconcilerRegistry{},
 	}
+
 	err := runtime.Start()
 	require.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+	central := &v1alpha1.Central{}
+	err = fakeClient.Get(context.TODO(), ctrlClient.ObjectKey{Name: "test-central", Namespace: "test-namespace"}, central)
+	require.NoError(t, err)
+	//assert.Len(t, fleetManagerClientMock.UpdateStatusCalls(), 1)
+
+	fleetManagerClientMock.GetManagedCentralListFunc = func() (*private.ManagedCentralList, error) {
+		return &private.ManagedCentralList{
+			Items: []private.ManagedCentral{
+				{
+					Id:   "id",
+					Kind: "",
+					Spec: private.ManagedCentralAllOfSpec{},
+					Metadata: private.ManagedCentralAllOfMetadata{
+						Name:              "test-central",
+						Namespace:         "test-namespace",
+						DeletionTimestamp: time.Now().String(),
+					},
+				},
+			},
+		}, nil
+	}
+
+	time.Sleep(5 * time.Second)
+	err = fakeClient.Get(context.TODO(), ctrlClient.ObjectKey{Name: "test-central", Namespace: "test-namespace"}, central)
+	require.True(t, apiErrors.IsNotFound(err))
 }
