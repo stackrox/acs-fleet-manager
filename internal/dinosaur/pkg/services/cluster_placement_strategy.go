bebff3b9f891ee86623ae2739f3c95036c43657e
Johannes Malsam <jmalsam@redhat.com>
2022-09-16 13:58:49 +0200

    implemented target cluster placement strategy

--- internal/dinosaur/pkg/services/cluster_placement_strategy.go
+++ internal/dinosaur/pkg/services/cluster_placement_strategy.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/api/dbapi"
 	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/config"
@@ -20,29 +21,33 @@ type ClusterPlacementStrategy interface {
 // placement configuration. An appropriate ClusterPlacementStrategy implementation
 // is returned based on the received parameters content
 func NewClusterPlacementStrategy(clusterService ClusterService, dataplaneClusterConfig *config.DataplaneClusterConfig) ClusterPlacementStrategy {
-	var clusterSelection ClusterPlacementStrategy = FirstDBClusterPlacementStrategy{
-		clusterService: clusterService,
+	var clusterSelection ClusterPlacementStrategy
+	if dataplaneClusterConfig.DataPlaneClusterTarget != "" {
+		clusterSelection = TargetClusterPlacementStrategy{
+			targetClusterID: dataplaneClusterConfig.DataPlaneClusterTarget,
+			clusterService:  clusterService}
+	} else {
+		clusterSelection = DefaultClusterPlacementStrategy{
+			clusterService: clusterService,
+		}
 	}
 
 	return clusterSelection
 }
 
 // TODO(create-ticket): Revisit placement strategy before going live.
-var _ ClusterPlacementStrategy = (*FirstDBClusterPlacementStrategy)(nil)
+var _ ClusterPlacementStrategy = (*DefaultClusterPlacementStrategy)(nil)
 
-// FirstDBClusterPlacementStrategy ...
-type FirstDBClusterPlacementStrategy struct {
+// DefaultClusterPlacementStrategy ...
+type DefaultClusterPlacementStrategy struct {
 	clusterService ClusterService
 }
 
 // FindCluster ...
-func (d FirstDBClusterPlacementStrategy) FindCluster(dinosaur *dbapi.CentralRequest) (*api.Cluster, error) {
-	clusters, err := d.clusterService.FindAllClusters(FindClusterCriteria{})
+func (d DefaultClusterPlacementStrategy) FindCluster(dinosaur *dbapi.CentralRequest) (*api.Cluster, error) {
+	clusters, err := findAllClusters(d.clusterService)
 	if err != nil {
 		return nil, err
-	}
-	if len(clusters) == 0 {
-		return nil, errors.New("no cluster was found")
 	}
 
 	for _, cluster := range clusters {
@@ -52,4 +57,40 @@ func (d FirstDBClusterPlacementStrategy) FindCluster(dinosaur *dbapi.CentralRequ
 	}
 
 	return nil, errors.New("no schedulable cluster found")
+}
+
+var _ ClusterPlacementStrategy = TargetClusterPlacementStrategy{}
+
+// TargetClusterPlacementStrategy implements the ClusterPlacementStrategy to always return the same cluster
+type TargetClusterPlacementStrategy struct {
+	targetClusterID string
+	clusterService  ClusterService
+}
+
+// FindCluster returns the target cluster of the placement strategy if found in the cluster list
+func (f TargetClusterPlacementStrategy) FindCluster(central *dbapi.CentralRequest) (*api.Cluster, error) {
+	clusters, err := findAllClusters(f.clusterService)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, cluster := range clusters {
+		if cluster.ID == f.targetClusterID {
+			return cluster, nil
+		}
+	}
+
+	return nil, fmt.Errorf("target cluster: %v not found in cluster list", f.targetClusterID)
+}
+
+func findAllClusters(c ClusterService) ([]*api.Cluster, error) {
+	clusters, err := c.FindAllClusters(FindClusterCriteria{})
+	if err != nil {
+		return nil, err
+	}
+	if len(clusters) == 0 {
+		return nil, errors.New("no cluster was found")
+	}
+
+	return clusters, nil
 }
