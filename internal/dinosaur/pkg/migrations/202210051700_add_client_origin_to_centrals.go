59c8aa3682d7970f821f5510f8443ba4cfb00ddd
Daniel Haus <dhaus@redhat.com>
2022-10-08 03:54:50 +0200

    Do not use default value for client origin, update the central requests after migrating the table schema.

--- internal/dinosaur/pkg/migrations/202210051700_add_client_origin_to_centrals.go
+++ internal/dinosaur/pkg/migrations/202210051700_add_client_origin_to_centrals.go
@@ -15,8 +15,7 @@ func addClientOriginToCentralRequest() *gormigrate.Migration {
 		ClientID     string `json:"idp_client_id"`
 		ClientSecret string `json:"idp_client_secret"`
 		Issuer       string `json:"idp_issuer"`
-		ClientOrigin string `json:"client_origin" gorm:"default:shared_static_rhsso"` // Currently only static clients
-		// have been used to provision centrals, hence we set it to shared_static_rhsso by default when no value previously existed.
+		ClientOrigin string `json:"client_origin"`
 	}
 
 	type CentralRequest struct {
@@ -57,13 +56,28 @@ func addClientOriginToCentralRequest() *gormigrate.Migration {
 		ID: "202210051700",
 		Migrate: func(tx *gorm.DB) error {
 			if err := tx.Migrator().AddColumn(&CentralRequest{}, "ClientOrigin"); err != nil {
-				return fmt.Errorf("adding new colum ClientOrigin in migration 202205101700: %w", err)
+				return fmt.Errorf("adding new colum ClientOrigin in migration 202210051700: %w", err)
+			}
+
+			// Update only the central requests which do not already specify client_origin after migrating the table
+			// schema. For central requests which do not specify it, we can assume shared_static_sso, since the field
+			// was not available before adding the possibility to create centrals with a dynamic OIDC client.
+			// **Note**: This update is _not_ done within a transaction since we pass `UseTransaction=false` as
+			// gormigrate option, meaning this will be on a best-effort basis, but the risk of not doing it within a
+			// transaction is minor.
+			err := tx.Model(&CentralRequest{}).
+				Where("client_origin IS NULL").
+				Update("client_origin", "shared_static_sso").Error
+
+			if err != nil {
+				return fmt.Errorf("setting shared_static_sso as initial value for new column "+
+					"ClientOrigin in migration 202210051700: %w", err)
 			}
 			return nil
 		},
 		Rollback: func(tx *gorm.DB) error {
 			if err := tx.Migrator().DropColumn(&CentralRequest{}, "ClientOrigin"); err != nil {
-				return fmt.Errorf("rolling back new column ClientOrigin in migration 202205101700: %w", err)
+				return fmt.Errorf("rolling back new column ClientOrigin in migration 202210051700: %w", err)
 			}
 			return nil
 		},
