0c6733dd07e2a35f816edbd7b3fc29953591b857
Simon Bäumer <baeumer@pm.me>
2022-10-25 16:46:58 +0200

    Update internal/dinosaur/pkg/defaults/default_scanner_spec.go
    Co-authored-by: Moritz Clasmeier <mtesseract@users.noreply.github.com>

--- internal/dinosaur/pkg/defaults/default_scanner_spec.go
+++ internal/dinosaur/pkg/defaults/default_scanner_spec.go
@@ -20,7 +20,7 @@ type AnalyzerDefaults struct {
 
 // DbDefaults ...
 type DbDefaults struct {
-	MemoryRequest resource.Quantity `env:"MEMORY_REQUEST" envDefault:"500Mi"`
+	MemoryRequest resource.Quantity `env:"MEMORY_REQUEST" envDefault:"500M"`
 	CPURequest    resource.Quantity `env:"CPU_REQUEST"    envDefault:"10m"`
 	MemoryLimit   resource.Quantity `env:"MEMORY_LIMIT"   envDefault:"2500Mi"`
 	CPULimit      resource.Quantity `env:"CPU_LIMIT"      envDefault:"250m"`
