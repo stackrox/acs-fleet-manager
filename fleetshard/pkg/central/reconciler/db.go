a90eb1ba60d7b413b4fc76c47253e7aa38331c42
Vlad Bologa <vbologa@redhat.com>
2022-11-07 17:51:20 +0100

    Fix linter warnings

--- fleetshard/pkg/central/reconciler/db.go
+++ fleetshard/pkg/central/reconciler/db.go
@@ -15,14 +15,14 @@ import (
 )
 
 const (
-	DBCreatingStatus  = "creating"
-	DBAvailableStatus = "available"
+	dbCreatingStatus  = "creating"
+	dbAvailableStatus = "available"
 
-	AWSRegion       = "us-east-1" // TODO: this should not be hardcoded
-	DBEngine        = "aurora-postgresql"
-	DBEngineVersion = "13.7"
-	DBInstanceClass = "db.serverless"
-	DBUser          = "rhacs_master"
+	awsRegion       = "us-east-1" // TODO: this should not be hardcoded
+	dbEngine        = "aurora-postgresql"
+	dbEngineVersion = "13.7"
+	dbInstanceClass = "db.serverless"
+	dbUser          = "rhacs_master"
 )
 
 func ensureDBProvisioned(ctx context.Context, client ctrlClient.Client, remoteCentralNamespace string) (string, error) {
@@ -31,20 +31,20 @@ func ensureDBProvisioned(ctx context.Context, client ctrlClient.Client, remoteCe
 		return "", fmt.Errorf("unable to create RDS client, %v", err)
 	}
 
-	clusterId := remoteCentralNamespace + "-db-cluster"
-	instanceId := remoteCentralNamespace + "-db-instance"
+	clusterID := remoteCentralNamespace + "-db-cluster"
+	instanceID := remoteCentralNamespace + "-db-instance"
 
-	err = ensureDBClusterCreated(ctx, client, rdsClient, clusterId, remoteCentralNamespace)
+	err = ensureDBClusterCreated(ctx, client, rdsClient, clusterID, remoteCentralNamespace)
 	if err != nil {
-		return "", fmt.Errorf("ensuring DB cluster %s exists", clusterId)
+		return "", fmt.Errorf("ensuring DB cluster %s exists", clusterID)
 	}
 
-	err = ensureDBInstanceCreated(rdsClient, instanceId, clusterId)
+	err = ensureDBInstanceCreated(rdsClient, instanceID, clusterID)
 	if err != nil {
-		return "", fmt.Errorf("ensuring DB instance %s exists in cluster %s", instanceId, clusterId)
+		return "", fmt.Errorf("ensuring DB instance %s exists in cluster %s", instanceID, clusterID)
 	}
 
-	return waitForInstanceToBeAvailable(rdsClient, instanceId, clusterId)
+	return waitForInstanceToBeAvailable(rdsClient, instanceID, clusterID)
 }
 
 func ensureDBDeprovisioned(remoteCentralNamespace string) (bool, error) {
@@ -53,21 +53,21 @@ func ensureDBDeprovisioned(remoteCentralNamespace string) (bool, error) {
 		return false, fmt.Errorf("unable to create RDS client, %v", err)
 	}
 
-	clusterId := remoteCentralNamespace + "-db-cluster"
-	instanceId := remoteCentralNamespace + "-db-instance"
+	clusterID := remoteCentralNamespace + "-db-cluster"
+	instanceID := remoteCentralNamespace + "-db-instance"
 
 	//TODO: do not skip taking a final DB snapshot
-	if instanceExists(rdsClient, instanceId) {
+	if instanceExists(rdsClient, instanceID) {
 		//TODO: don't delete if state is "deleting"
-		_, err = rdsClient.DeleteDBInstance(newDeleteCentralDBInstanceInput(instanceId, true))
+		_, err = rdsClient.DeleteDBInstance(newDeleteCentralDBInstanceInput(instanceID, true))
 		if err != nil {
 			return false, fmt.Errorf("deleting DB instance: %v", err)
 		}
 	}
 
-	if clusterExists(rdsClient, clusterId) {
+	if clusterExists(rdsClient, clusterID) {
 		//TODO: don't delete if state is "deleting"
-		_, err = rdsClient.DeleteDBCluster(newDeleteCentralDBClusterInput(clusterId, true))
+		_, err = rdsClient.DeleteDBCluster(newDeleteCentralDBClusterInput(clusterID, true))
 		if err != nil {
 			return false, fmt.Errorf("deleting DB cluster: %v", err)
 		}
@@ -78,7 +78,7 @@ func ensureDBDeprovisioned(remoteCentralNamespace string) (bool, error) {
 
 func newRdsClient() (*rds.RDS, error) {
 	cfg := &aws.Config{
-		Region: aws.String(AWSRegion),
+		Region: aws.String(awsRegion),
 	}
 
 	sess, err := session.NewSession(cfg)
@@ -89,12 +89,15 @@ func newRdsClient() (*rds.RDS, error) {
 	return rds.New(sess), nil
 }
 
-func ensureDBClusterCreated(ctx context.Context, client ctrlClient.Client, rdsClient *rds.RDS, clusterId string, remoteCentralNamespace string) error {
-	if !clusterExists(rdsClient, clusterId) {
+func ensureDBClusterCreated(ctx context.Context, client ctrlClient.Client, rdsClient *rds.RDS, clusterID string, remoteCentralNamespace string) error {
+	if !clusterExists(rdsClient, clusterID) {
 		// cluster does not exist, create it
 		glog.Infof("Provisioning RDS database cluster.")
 		dbPassword, err := getDBPassword(ctx, client, remoteCentralNamespace)
-		_, err = rdsClient.CreateDBCluster(newCreateCentralDBClusterInput(clusterId, dbPassword))
+		if err != nil {
+			return fmt.Errorf("getting password for DB cluster: %v", err)
+		}
+		_, err = rdsClient.CreateDBCluster(newCreateCentralDBClusterInput(clusterID, dbPassword))
 		if err != nil {
 			return fmt.Errorf("creating DB cluster: %v", err)
 		}
@@ -103,11 +106,11 @@ func ensureDBClusterCreated(ctx context.Context, client ctrlClient.Client, rdsCl
 	return nil
 }
 
-func ensureDBInstanceCreated(rdsClient *rds.RDS, instanceId string, clusterId string) error {
-	if !instanceExists(rdsClient, instanceId) {
+func ensureDBInstanceCreated(rdsClient *rds.RDS, instanceID string, clusterID string) error {
+	if !instanceExists(rdsClient, instanceID) {
 		// instance does not exist, create it
 		glog.Infof("Provisioning RDS database instance.")
-		_, err := rdsClient.CreateDBInstance(newCreateCentralDBInstanceInput(clusterId, instanceId))
+		_, err := rdsClient.CreateDBInstance(newCreateCentralDBInstanceInput(clusterID, instanceID))
 		if err != nil {
 			// TODO: delete cluster
 			return fmt.Errorf("creating DB instance: %v", err)
@@ -117,41 +120,33 @@ func ensureDBInstanceCreated(rdsClient *rds.RDS, instanceId string, clusterId st
 	return nil
 }
 
-func clusterExists(rdsClient *rds.RDS, clusterId string) bool {
+func clusterExists(rdsClient *rds.RDS, clusterID string) bool {
 	dbClusterQuery := &rds.DescribeDBClustersInput{
-		DBClusterIdentifier: aws.String(clusterId),
+		DBClusterIdentifier: aws.String(clusterID),
 	}
 
 	_, err := rdsClient.DescribeDBClusters(dbClusterQuery)
-	if err != nil {
-		return false
-	}
-
-	return true
+	return err == nil
 }
 
-func instanceExists(rdsClient *rds.RDS, instanceId string) bool {
+func instanceExists(rdsClient *rds.RDS, instanceID string) bool {
 	dbInstanceQuery := &rds.DescribeDBInstancesInput{
-		DBInstanceIdentifier: aws.String(instanceId),
+		DBInstanceIdentifier: aws.String(instanceID),
 		//TODO: add cluster Filter
 	}
 
 	_, err := rdsClient.DescribeDBInstances(dbInstanceQuery)
-	if err != nil {
-		return false
-	}
-
-	return true
+	return err == nil
 }
 
-func waitForInstanceToBeAvailable(rdsClient *rds.RDS, instanceId string, clusterId string) (string, error) {
+func waitForInstanceToBeAvailable(rdsClient *rds.RDS, instanceID string, clusterID string) (string, error) {
 	dbInstanceQuery := &rds.DescribeDBInstancesInput{
-		DBInstanceIdentifier: aws.String(instanceId),
+		DBInstanceIdentifier: aws.String(instanceID),
 		//TODO: add cluster Filter
 	}
 
 	dbClusterQuery := &rds.DescribeDBClustersInput{
-		DBClusterIdentifier: aws.String(clusterId),
+		DBClusterIdentifier: aws.String(clusterID),
 	}
 
 	for {
@@ -165,7 +160,7 @@ func waitForInstanceToBeAvailable(rdsClient *rds.RDS, instanceId string, cluster
 		}
 
 		dbInstanceStatus := *result.DBInstances[0].DBInstanceStatus
-		if dbInstanceStatus == DBAvailableStatus {
+		if dbInstanceStatus == dbAvailableStatus {
 
 			clusterResult, err := rdsClient.DescribeDBClusters(dbClusterQuery)
 			if err != nil {
@@ -173,17 +168,17 @@ func waitForInstanceToBeAvailable(rdsClient *rds.RDS, instanceId string, cluster
 			}
 
 			connectionString := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=require",
-				*clusterResult.DBClusters[0].Endpoint, 5432, DBUser, "postgres")
+				*clusterResult.DBClusters[0].Endpoint, 5432, dbUser, "postgres")
 
 			return connectionString, nil
-		} else {
-			// TODO: creating is not the only valid status
-			if dbInstanceStatus != DBCreatingStatus {
-				//TODO: cleanup
-				return "", fmt.Errorf("unexpected instance status: %s", dbInstanceStatus)
-			}
-			time.Sleep(10 * time.Second)
 		}
+
+		// TODO: creating is not the only valid status
+		if dbInstanceStatus != dbCreatingStatus {
+			//TODO: cleanup
+			return "", fmt.Errorf("unexpected instance status: %s", dbInstanceStatus)
+		}
+		time.Sleep(10 * time.Second)
 	}
 }
 
@@ -205,13 +200,13 @@ func getDBPassword(ctx context.Context, client ctrlClient.Client, remoteCentralN
 	return "", fmt.Errorf("central DB secret does not contain password field: %v", err)
 }
 
-func newCreateCentralDBClusterInput(clusterId string, dbPassword string) *rds.CreateDBClusterInput {
+func newCreateCentralDBClusterInput(clusterID string, dbPassword string) *rds.CreateDBClusterInput {
 	return &rds.CreateDBClusterInput{
-		DBClusterIdentifier: aws.String(clusterId),
-		Engine:              aws.String(DBEngine),
-		EngineVersion:       aws.String(DBEngineVersion),
-		MasterUsername:      aws.String(DBUser),
-		MasterUserPassword:  aws.String(dbPassword), // TODO: generate random password
+		DBClusterIdentifier: aws.String(clusterID),
+		Engine:              aws.String(dbEngine),
+		EngineVersion:       aws.String(dbEngineVersion),
+		MasterUsername:      aws.String(dbUser),
+		MasterUserPassword:  aws.String(dbPassword),
 		// TODO: the security group needs to be created during the data plane terraforming, and made known to
 		// fleet-manager via a configuration parameter. I created this one in the AWS Console.
 		VpcSecurityGroupIds: aws.StringSlice([]string{"sg-04dcc23a03646041c"}),
@@ -232,26 +227,26 @@ func newCreateCentralDBClusterInput(clusterId string, dbPassword string) *rds.Cr
 	}
 }
 
-func newCreateCentralDBInstanceInput(clusterId, instanceId string) *rds.CreateDBInstanceInput {
+func newCreateCentralDBInstanceInput(clusterID, instanceID string) *rds.CreateDBInstanceInput {
 	return &rds.CreateDBInstanceInput{
-		DBInstanceClass:      aws.String(DBInstanceClass),
-		DBClusterIdentifier:  aws.String(clusterId),
-		DBInstanceIdentifier: aws.String(instanceId),
-		Engine:               aws.String(DBEngine),
+		DBInstanceClass:      aws.String(dbInstanceClass),
+		DBClusterIdentifier:  aws.String(clusterID),
+		DBInstanceIdentifier: aws.String(instanceID),
+		Engine:               aws.String(dbEngine),
 		PubliclyAccessible:   aws.Bool(true), // TODO: should be false
 	}
 }
 
-func newDeleteCentralDBInstanceInput(instanceId string, skipFinalSnapshot bool) *rds.DeleteDBInstanceInput {
+func newDeleteCentralDBInstanceInput(instanceID string, skipFinalSnapshot bool) *rds.DeleteDBInstanceInput {
 	return &rds.DeleteDBInstanceInput{
-		DBInstanceIdentifier: aws.String(instanceId),
+		DBInstanceIdentifier: aws.String(instanceID),
 		SkipFinalSnapshot:    aws.Bool(skipFinalSnapshot),
 	}
 }
 
-func newDeleteCentralDBClusterInput(clusterId string, skipFinalSnapshot bool) *rds.DeleteDBClusterInput {
+func newDeleteCentralDBClusterInput(clusterID string, skipFinalSnapshot bool) *rds.DeleteDBClusterInput {
 	return &rds.DeleteDBClusterInput{
-		DBClusterIdentifier: aws.String(clusterId),
+		DBClusterIdentifier: aws.String(clusterID),
 		SkipFinalSnapshot:   aws.Bool(skipFinalSnapshot),
 	}
 }
