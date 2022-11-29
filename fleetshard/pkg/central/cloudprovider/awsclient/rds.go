8b1f3e3fd276fdf7249248deca6335ba3706155c
Vlad Bologa <vbologa@redhat.com>
2022-11-29 14:13:57 +0100

    Make sure DB is not publicly accessible

--- fleetshard/pkg/central/cloudprovider/awsclient/rds.go
+++ fleetshard/pkg/central/cloudprovider/awsclient/rds.go
@@ -280,7 +280,7 @@ func newCreateCentralDBInstanceInput(clusterID, instanceID string) *rds.CreateDB
 		DBClusterIdentifier:  aws.String(clusterID),
 		DBInstanceIdentifier: aws.String(instanceID),
 		Engine:               aws.String(dbEngine),
-		PubliclyAccessible:   aws.Bool(true), // TODO: must be set to false after VPC peering is done
+		PubliclyAccessible:   aws.Bool(false),
 	}
 }
 
