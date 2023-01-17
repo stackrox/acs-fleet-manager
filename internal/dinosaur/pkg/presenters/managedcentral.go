d4de9bcad28d2614f6c66d244331c3104de79970
dhaus67 <dhaus@redhat.com>
2023-01-17 20:33:38 +0100

    Revert "Revert "ROX-12344: Add organisation name to central request" (#736)"
    This reverts commit f4e71a7fd5493ae39090ad2feddbf729709b7e1d.

--- internal/dinosaur/pkg/presenters/managedcentral.go
+++ internal/dinosaur/pkg/presenters/managedcentral.go
@@ -69,6 +69,7 @@ func (c *ManagedCentralPresenter) PresentManagedCentral(from *dbapi.CentralReque
 				ClientSecret: from.AuthConfig.ClientSecret, // pragma: allowlist secret
 				ClientOrigin: from.AuthConfig.ClientOrigin,
 				OwnerOrgId:   from.OrganisationID,
+				OwnerOrgName: from.OrganisationName,
 				OwnerUserId:  from.OwnerUserID,
 				Issuer:       from.AuthConfig.Issuer,
 			},
