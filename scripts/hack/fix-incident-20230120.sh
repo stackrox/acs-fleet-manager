f679c2994b981e166d563f8a59f76eea2a9635a0
Daniel Haus <dhaus@redhat.com>
2023-01-24 12:19:33 +0100

    Remove --migrate-admin flag.

--- scripts/hack/fix-incident-20230120.sh
+++ scripts/hack/fix-incident-20230120.sh
@@ -21,7 +21,6 @@ MANDATORY:
     --central-list-path     The path to the with list of central instances that should be fixed.
 
 OPTION:
-    --migrate-org-admin     The additional migration flag for ROX-13944.
     --help                  Prints help information.
 
 Example:
@@ -54,19 +53,13 @@ function migrate_all_centrals() {
     local central_list_path="${1:-}"
     [[ "${central_list_path}" = "" ]] && log "Error: Parameter 'central_list_path' is empty." && usage_exit
 
-    local migrate_org_admin="${2:-false}"
-
     local script_dir
     script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
     local binary_file_path
     binary_file_path=$(realpath "${script_dir}/../../dataplane-migrators")
 
-    # 1. prepare conditional params for AWS CLI command
-    local conditional_params=()
-    [[ "${migrate_org_admin}" == "true" ]] && conditional_params+=("--migrate-org-admin")
-
-    # 2. read CSV file and process central one by one. So that we can log results nicely.
+    # 1. read CSV file and process central one by one. So that we can log results nicely.
     # Used query:
     # ./stage-action.sh gabi "SELECT id,name,organisation_id,CONCAT('https://acs-',id,'.',host),client_id,client_secret FROM central_requests WHERE deleted_at IS NULL ORDER BY created_at DESC;" | jq '.result[1:][] | @csv ' --raw-output > issue-20230120-stage.csv
     while read -r csv_line; do
@@ -90,7 +83,6 @@ function migrate_all_centrals() {
             --name "${csv_name}" \
             --org-id "${csv_organisation_id}" \
             --url "${csv_host}" \
-            "${conditional_params[@]}"
 
         # execute migration
         ${binary_file_path} incident-20230120 \
@@ -100,7 +92,6 @@ function migrate_all_centrals() {
             --name "${csv_name}" \
             --org-id "${csv_organisation_id}" \
             --url "${csv_host}" \
-            "${conditional_params[@]}"
 
         echo ">>> Done"
     done <"${central_list_path}"
@@ -110,16 +101,12 @@ function migrate_all_centrals() {
 
 function main() {
     local central_list_path=""
-    local migrate_org_admin="false"
 
     while [[ -n "${1:-}" ]]; do
         case "${1}" in
         "--central-list-path")
             central_list_path="${2:-}"
             shift
-            ;;
-        "--migrate-org-admin")
-            migrate_org_admin="true"
             ;;
         "--help")
             usage_exit
@@ -143,7 +130,7 @@ function main() {
     central_list_path=$(realpath "${central_list_path}")
     [[ ! -f "${central_list_path}" ]] && log "Error: File provided in option '--central-list-path' does not exist." && usage_exit
 
-    migrate_all_centrals "${central_list_path}" "${migrate_org_admin}"
+    migrate_all_centrals "${central_list_path}"
 }
 
 main "$@"
