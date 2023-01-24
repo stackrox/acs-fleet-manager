b2f63ba1c136a0c56058eda3381606d5a1dc4e50
Stephan Hesselmann <shesselm@redhat.com>
2023-01-24 13:30:00 +0100

    ROX-13593: Store telemetry storage key as secret (#747)

--- pkg/client/telemetry/config.go
+++ pkg/client/telemetry/config.go
@@ -4,33 +4,45 @@ package telemetry
 import (
 	"os"
 
+	"github.com/golang/glog"
 	"github.com/spf13/pflag"
+	"github.com/stackrox/acs-fleet-manager/pkg/shared"
 	"github.com/stackrox/rox/pkg/telemetry/phonehome"
 )
 
 // TelemetryConfig is a wrapper for the telemetry configuration.
 type TelemetryConfig struct {
 	phonehome.Config
+
+	StorageKeyFile string
 }
 
 // NewTelemetryConfig creates a new telemetry configuration.
 func NewTelemetryConfig() *TelemetryConfig {
 	// HOSTNAME is set to the pod name by K8s.
 	clientID := getEnv("HOSTNAME", "fleet-manager")
-	return &TelemetryConfig{phonehome.Config{
-		ClientID:   clientID,
-		ClientName: "ACS Fleet Manager",
-	}}
+	return &TelemetryConfig{
+		Config: phonehome.Config{
+			ClientID:   clientID,
+			ClientName: "ACS Fleet Manager",
+		},
+		StorageKeyFile: "secrets/telemetry.storageKey",
+	}
 }
 
 // AddFlags adds telemetry CLI flags.
 func (t *TelemetryConfig) AddFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&t.Endpoint, "telemetry-endpoint", t.Config.Endpoint, "The telemetry endpoint")
-	fs.StringVar(&t.StorageKey, "telemetry-storage-key", t.Config.StorageKey, "The telemetry storage key")
+	fs.StringVar(&t.Endpoint, "telemetry-endpoint", t.Endpoint, "The telemetry endpoint")
+	fs.StringVar(&t.StorageKeyFile, "telemetry-storage-key-secret-file", t.StorageKeyFile, "File containing the telemetry storage key")
 }
 
 // ReadFiles reads telemetry secret files.
 func (t *TelemetryConfig) ReadFiles() error {
+	err := shared.ReadFileValueString(t.StorageKeyFile, &t.StorageKey)
+	// Don't fail if telemetry secret key is not found.
+	if err != nil {
+		glog.Warningf("could not read telemetry storage key secret file")
+	}
 	return nil
 }
 
