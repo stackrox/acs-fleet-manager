f8b4163825d8a298dfff3a3b6ab15057fa5fb654
Stephan Heßelmann <shesselm@redhat.com>
2023-02-08 17:23:57 +0100

    add comment about temporary usage

--- internal/dinosaur/pkg/services/data_migration.go
+++ internal/dinosaur/pkg/services/data_migration.go
@@ -10,6 +10,11 @@ import (
 )
 
 // DataMigration is the database migration boot service.
+// The data migration is executed on boot and is non-blocking. Its purpose is to migrate
+// existing data records, which have the `organisation_name` column not yet set.
+// Once all records have this column set in production, the data migration service
+// becomes obsolete and will be removed again.
+// TODO (2023-03/stehessel): remove data migration service.
 type DataMigration struct {
 	connectionFactory *db.ConnectionFactory
 	amsClient         ocm.AMSClient
