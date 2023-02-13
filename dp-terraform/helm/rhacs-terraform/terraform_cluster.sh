d9dc740122d326e31ebbb5095a6cc04095d652ba
Stephan Heßelmann <shesselm@redhat.com>
2023-02-13 16:45:06 +0100

    stage central: v3.73.0-nightly-20230203 -> v3.74.0-nightly-20230213

--- dp-terraform/helm/rhacs-terraform/terraform_cluster.sh
+++ dp-terraform/helm/rhacs-terraform/terraform_cluster.sh
@@ -35,7 +35,7 @@ case $ENVIRONMENT in
     OBSERVABILITY_OBSERVATORIUM_GATEWAY="https://observatorium-mst.api.stage.openshift.com"
     OBSERVABILITY_OPERATOR_VERSION="v4.0.4"
     OPERATOR_USE_UPSTREAM="true"
-    OPERATOR_VERSION="v3.73.0-nightly-20230203"
+    OPERATOR_VERSION="v3.74.0-nightly-20230213"
     ;;
 
   prod)
