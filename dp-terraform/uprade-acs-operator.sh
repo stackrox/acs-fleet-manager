d6130caf9c5da64a63456c07874467868c951709
Simon Baeumer <sbaumer@redhat.com>
2023-03-10 11:00:53 +0100

    Finish upgrade script

--- dp-terraform/uprade-acs-operator.sh
+++ dp-terraform/uprade-acs-operator.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-set -exo pipefail
+set -eo pipefail
 
 # This script is a small canary upgrade implementation for the ACS operator.
 # It annotates all instances with a pause-reconcile annotation, upgrades the ACS operator.
 # Afterwards the user can decide which instances to upgrade.
 
+# upgrade_operator will pause all central instances and approve an install plan to upgrade the operator via OLM.
+# The script asks for confirmation before upgrading.
+# An upgrade is executed by unpausing a Central instance. This must be done manually.
 upgrade_operator() {
-    central_namespaces=$(kubectl get centrals.platform.stackrox.io -A | awk '(NR>1) {print $1}')
-    if [[ -z "$central_namespaces" ]]; then
-        echo "no central instance found"
-        exit 1;
+    echo "Starting Operator upgrade (this will pause reconciliation on all Centrals) [yes/no]? "
+    read -r answer_upgrade
+    if [[ "$answer_upgrade" != "yes" ]]; then
+        echo "User aborted."
+        exit 1
     fi
 
-    for ns in $central_namespaces
-    do
-        central_name=$(kubectl get centrals.platform.stackrox.io -n "$ns" | awk '(NR>1) {print $1}')
-        kubectl -n "$ns" annotate centrals.platform.stackrox.io "$central_name" stackrox.io/pause-reconcile=true
-        echo "Paused: $central_name"
-    done
+    pause_all_centrals
 
-    echo "Executing Operator upgrade"
+    list_paused_centrals
+
+    echo "Start executing Operator upgrade"
 
     # receive install plan
     install_plan=$(kubectl get installplans.operators.coreos.com -A -o json | jq '.items[] | select(.spec.approved == false) | select(.spec.clusterServiceVersionNames[])')
@@ -34,14 +35,14 @@ upgrade_operator() {
     fi
 
     echo "Approving installplan $install_plan_namespace/$install_plan_name to $install_plan_version? [yes/no]"
-    read apporve_install_answer
-    if [[ "$approve_install_answer" -ne "yes" ]]; then
+    read -r approve_install_answer
+    if [[ "$approve_install_answer" != "yes" ]]; then
         echo "Aborting operator upgrade. User interrupt."
         exit 1
     fi
 
-    echo "To confirm installation type \""$install_plan_version"\":"
-    read confirm_install_answer
+    echo "To confirm installation type \"$install_plan_version\":"
+    read -r confirm_install_answer
     if [[ "$confirm_install_answer" = "$install_plan_version" ]]; then
         echo "Upgrade confirmed by user."
     else
@@ -49,15 +50,71 @@ upgrade_operator() {
         exit 1
     fi
 
-    kubectl patch installplan -n "$install_plan_namespace" $install_plan_name -p '{"spec":{"approved": true}}' --type merge
+    kubectl patch installplan -n "$install_plan_namespace" "$install_plan_name" -p '{"spec":{"approved": true}}' --type merge
+
+    echo "Operator upgraded. Please check Operator health manually."
+    sleep 1
+    echo "Central instance upgrades must be performed manually by unpausing instances to be reconciled by the new operator version."
 }
 
-## TODO: implement upgrade first canary
-## TODO: implement upgrade all other instances
-## TODO: list paused instances
-upgrade_instance() {
-    id="$1"
-    echo $id
+# pause_all_centrals sets all "stackrox.io/pause-reconcile" annotations to "true"
+pause_all_centrals() {
+    central_namespaces=$(get_central_namespaces)
+    for ns in $central_namespaces
+    do
+      central_name=$(kubectl get centrals.platform.stackrox.io -n "$ns" | awk '(NR>1) {print $1}')
+      kubectl -n "$ns" annotate centrals.platform.stackrox.io "$central_name" stackrox.io/pause-reconcile=true --overwrite=true
+      echo "Paused: $central_name"
+    done
+}
+
+# unpause_all_centrals sets all "stackrox.io/pause-reconcile" annotations to "false"
+unpause_all_centrals() {
+    central_namespaces=$(get_central_namespaces)
+    for ns in $central_namespaces
+    do
+        central_name=$(kubectl get centrals.platform.stackrox.io -n "$ns" | awk '(NR>1) {print $1}')
+        kubectl -n "$ns" annotate centrals.platform.stackrox.io "$central_name" stackrox.io/pause-reconcile=false --overwrite=true
+        echo "Unpaused: $central_name"
+    done
+}
+
+# list_paused_centrals lists all Central instances in a cluster which are annotated as "stackrox.io/pause-reconcile=true"
+list_paused_centrals() {
+    central_namespaces=$(get_central_namespaces)
+    central_count=$(echo "$central_namespaces" | wc -l)
+
+    paused_counter=0
+    for ns in $central_namespaces
+    do
+      central=$(kubectl get centrals.platform.stackrox.io -n "$ns" -o json | jq '.items[0]')
+      if [[ "$central" == "null" ]]; then
+        continue
+      fi
+
+      is_paused=$(echo "$central" | jq '.metadata.annotations["stackrox.io/pause-reconcile"]' -r)
+      if [[ "$is_paused" != "true" ]]; then
+          continue
+      fi
+
+      echo "$central" | jq '{namespace: .metadata.namespace, name: .metadata.name}' -r
+      paused_counter=$((paused_counter+1))
+    done
+    if [[ "$paused_counter" != "0" ]]; then
+        echo ""
+    fi
+
+    central_count=$(echo "$central_count" | xargs) #remove whitespace
+    echo "$paused_counter/$central_count Central instances are paused."
+}
+
+get_central_namespaces() {
+    central_namespaces=$(kubectl get centrals.platform.stackrox.io -A | awk '(NR>1) {print $1}')
+    if [[ -z "$central_namespaces" ]]; then
+        echo "no central instance found"
+        exit 1;
+    fi
+    echo "$central_namespaces"
 }
 
 case $1 in
@@ -65,13 +122,17 @@ case $1 in
     shift
     upgrade_operator "$@"
     ;;
-  upgrade_instance)
+  unpause_all_centrals)
     shift
-    upgrade_instance "$@"
+    unpause_all_centrals "$@"
     ;;
-  upgrade_all)
+  pause_all_centrals)
     shift
-    upgrade_instance "$@"
+    pause_all_centrals "$@"
+    ;;
+  list_paused_centrals)
+    shift
+    list_paused_centrals "$@"
     ;;
   *)
     echo "unknown argument $1"
