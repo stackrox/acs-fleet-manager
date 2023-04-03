5226dffecb069088c75aa4dd330f5496313d426a
Yury Kovalev <ykovalev@redhat.com>
2023-04-03 15:42:23 +0200

    Add missing AWS_ROLE_ARN parameter in dev script for fleetshard-sync

--- dev/env/scripts/exec_fleetshard_sync.sh
+++ dev/env/scripts/exec_fleetshard_sync.sh
@@ -2,21 +2,30 @@
 
 set -euo pipefail
 
-CLUSTER_NAME="cluster-acs-dev-dp-01"
 GITROOT="$(git rev-parse --show-toplevel)"
-
-export AWS_AUTH_HELPER="${AWS_AUTH_HELPER:-aws-vault}"
-
-# shellcheck source=scripts/lib/external_config.sh
-source "${GITROOT}/scripts/lib/external_config.sh"
-init_chamber
+ENABLE_EXTERNAL_CONFIG="${ENABLE_EXTERNAL_CONFIG:-true}"
 
 ARGS="${GITROOT}/fleetshard-sync"
 if [[ "$#" -gt 0 ]]; then
     ARGS="$*"
 fi
-# shellcheck disable=SC2086
-CLUSTER_ID=$(run_chamber read "${CLUSTER_NAME}" ID -q) \
-MANAGED_DB_SECURITY_GROUP=$(run_chamber read "${CLUSTER_NAME}" MANAGED_DB_SECURITY_GROUP -q) \
-MANAGED_DB_SUBNET_GROUP=$(run_chamber read "${CLUSTER_NAME}" MANAGED_DB_SUBNET_GROUP -q) \
-run_chamber exec fleetshard-sync -b secretsmanager -- ${ARGS}
+
+if [[ "$ENABLE_EXTERNAL_CONFIG" != "true" ]]; then
+    ${ARGS}
+    return
+fi
+
+export AWS_AUTH_HELPER="${AWS_AUTH_HELPER:-aws-vault}"
+# shellcheck source=scripts/lib/external_config.sh
+source "${GITROOT}/scripts/lib/external_config.sh"
+init_chamber
+
+CLUSTER_NAME="cluster-acs-dev-dp-01"
+
+ARGS="CLUSTER_ID=$(run_chamber read ${CLUSTER_NAME} ID -q -b ssm) \
+    MANAGED_DB_SECURITY_GROUP=$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SECURITY_GROUP -q -b ssm) \
+    MANAGED_DB_SUBNET_GROUP=$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SUBNET_GROUP -q -b ssm) \
+    AWS_ROLE_ARN=$(run_chamber read fleetshard-sync AWS_ROLE_ARN -q -b ssm) \
+    $ARGS"
+
+run_chamber exec fleetshard-sync -b secretsmanager -- sh -c "$ARGS"
