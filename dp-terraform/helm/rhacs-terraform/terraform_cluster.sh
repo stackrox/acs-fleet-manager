9f5782450a753e713d0f00d883ad7624339c1cf5
Aleksandr Kurlov <akurlov@redhat.com>
2023-04-25 10:35:38 +0200

    Bump operator to v4.0.0

--- dp-terraform/helm/rhacs-terraform/terraform_cluster.sh
+++ dp-terraform/helm/rhacs-terraform/terraform_cluster.sh
@@ -36,8 +36,8 @@ case $ENVIRONMENT in
     OBSERVABILITY_GITHUB_TAG="master"
     OBSERVABILITY_OBSERVATORIUM_GATEWAY="https://observatorium-mst.api.nonexistent.openshift.com"
     OBSERVABILITY_OPERATOR_VERSION="v4.2.1"
-    OPERATOR_USE_UPSTREAM="false"
-    OPERATOR_VERSION="v3.74.0"
+    OPERATOR_USE_UPSTREAM="true"
+    OPERATOR_VERSION="v4.0.0"
     ;;
 
   stage)
@@ -45,8 +45,8 @@ case $ENVIRONMENT in
     OBSERVABILITY_GITHUB_TAG="master"
     OBSERVABILITY_OBSERVATORIUM_GATEWAY="https://observatorium-mst.api.stage.openshift.com"
     OBSERVABILITY_OPERATOR_VERSION="v4.2.1"
-    OPERATOR_USE_UPSTREAM="false"
-    OPERATOR_VERSION="v3.74.0"
+    OPERATOR_USE_UPSTREAM="true"
+    OPERATOR_VERSION="v4.0.0"
     ;;
 
   prod)
