fc0f2be99a1d2929606cf984754fa2e857acb7cf
Michaël <smartptr@gmail.com>
2023-05-03 20:08:34 +0200

    Add `sleep(6s)` before tracking central creation (#999)

--- internal/dinosaur/pkg/handlers/admin_dinosaur.go
+++ internal/dinosaur/pkg/handlers/admin_dinosaur.go
@@ -81,8 +81,8 @@ func (h adminCentralHandler) Create(w http.ResponseWriter, r *http.Request) {
 		},
 		Action: func() (interface{}, *errors.ServiceError) {
 			svcErr := h.service.RegisterDinosaurJob(&convCentral)
-			h.telemetry.RegisterTenant(r.Context(), &convCentral)
-			h.telemetry.TrackCreationRequested(r.Context(), convCentral.ID, true, svcErr.AsError())
+			h.telemetry.RegisterTenant(ctx, &convCentral, true, svcErr.AsError())
+
 			if svcErr != nil {
 				return nil, svcErr
 			}
