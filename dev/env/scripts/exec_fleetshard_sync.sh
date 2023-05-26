398dc51a13ed812f6a6a2a697186ebc6da1d1813
Yury Kovalev <8366110+kovayur@users.noreply.github.com>
2023-05-26 13:46:44 +0200

    ROX-16161: Change the AWS authentication method in Fleetshard Sync (#984)

--- dev/env/scripts/exec_fleetshard_sync.sh
+++ dev/env/scripts/exec_fleetshard_sync.sh
@@ -22,10 +22,10 @@ init_chamber
 
 CLUSTER_NAME="cluster-acs-dev-dp-01"
 
-ARGS="CLUSTER_ID=$(run_chamber read ${CLUSTER_NAME} ID -q -b ssm) \
-    MANAGED_DB_SECURITY_GROUP=$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SECURITY_GROUP -q -b ssm) \
-    MANAGED_DB_SUBNET_GROUP=$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SUBNET_GROUP -q -b ssm) \
-    AWS_ROLE_ARN=$(run_chamber read fleetshard-sync AWS_ROLE_ARN -q -b ssm) \
+ARGS="CLUSTER_ID=${CLUSTER_ID:-$(run_chamber read ${CLUSTER_NAME} ID -q -b ssm)} \
+    MANAGED_DB_SECURITY_GROUP=${MANAGED_DB_SECURITY_GROUP:-$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SECURITY_GROUP -q -b ssm)} \
+    MANAGED_DB_SUBNET_GROUP=${MANAGED_DB_SUBNET_GROUP:-$(run_chamber read ${CLUSTER_NAME} MANAGED_DB_SUBNET_GROUP -q -b ssm)} \
+    AWS_ROLE_ARN=${FLEETSHARD_SYNC_AWS_ROLE_ARN:-$(run_chamber read fleetshard-sync AWS_ROLE_ARN -q -b ssm)} \
     $ARGS"
 
 run_chamber exec fleetshard-sync -b secretsmanager -- sh -c "$ARGS"
