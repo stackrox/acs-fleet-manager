11d6ceea837972da3bd3ff55c667174e289e5c84
Johannes Malsam <jmalsam@redhat.com>
2023-06-05 14:48:28 +0200

    introduce new main faile and make target

---
+++ cmd/acsfleetctl/main.go
@@ -0,0 +1,50 @@
+// main package for acsfleetctl CLI
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/spf13/cobra"
+	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/cmd/admin"
+	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/cmd/centrals"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:  "acsfleetctl",
+		Long: "acsfleetctl is a CLI used to interact with the ACSCS fleet-manager API",
+	}
+	rootCmd.PersistentFlags().Bool("debug", false, "use debug output")
+
+	setupSubCommands(rootCmd)
+
+	// This is used to recover from panics during initialization and glog.Fatal calls
+	// use the --debug flag to print stacktrace otherwise only the panic msg will be printed
+	defer recoverFromCLIPanic(rootCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Printf("Error executing command: %s", err)
+	}
+
+}
+
+func recoverFromCLIPanic(rootCmd *cobra.Command) {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+
+		dbg, err := rootCmd.Flags().GetBool("debug")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if dbg {
+			fmt.Println(string(debug.Stack()))
+		}
+	}
+}
+
+func setupSubCommands(rootCmd *cobra.Command) {
+	rootCmd.AddCommand(centrals.NewCentralsCommand())
+	rootCmd.AddCommand(admin.NewAdminCommand())
+}
