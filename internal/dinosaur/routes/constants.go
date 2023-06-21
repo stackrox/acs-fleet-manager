7ab06bc14fb6f890a9149e35bba4b1415d2371e6
Simon Baeumer <sbaumer@redhat.com>
2023-06-21 14:54:19 +0200

    WIP

--- internal/dinosaur/routes/constants.go
+++ internal/dinosaur/routes/constants.go
@@ -5,5 +5,5 @@ package routes
 const (
 	Version                           = "v1"
 	APIEndpoint                       = "/api"
-	DinosaursFleetManagementAPIPrefix = "rhacs" // TODO rename to correpond to your own fleet manager api prefix
+	DinosaursFleetManagementAPIPrefix = "rhacs"
 )
