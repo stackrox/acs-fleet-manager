a72b6e6921942a8b7048d82e25e54edd8b616227
Johannes Malsam <jmalsam@redhat.com>
2023-07-18 11:00:42 +0200

    add cipher package

---
+++ fleetshard/pkg/cipher/cipher.go
@@ -0,0 +1,74 @@
+// Package cipher defines encryption and decryption methods used by fleetshard-sync
+package cipher
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+const keySize = 32
+
+// Cipher is the interface used to encrypt and decrypt content
+type Cipher interface {
+	Encrypt(plaintext []byte) ([]byte, error)
+	Decrypt(ciphertext []byte) ([]byte, error)
+}
+
+// AES256Cipher implements encryption and decryption using AES256 GCM
+type AES256Cipher struct {
+	aesgcm cipher.AEAD
+}
+
+// NewAES256Cipher returns a new Cipher using the given key
+func NewAES256Cipher(key []byte) (Cipher, error) {
+	if len(key) != 32 {
+		return nil, fmt.Errorf("creating AES256Cipher, key does not match required lenght of %d", keySize)
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("creating cipher block: %s", err)
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("creating AES GCM cipher %s", err)
+	}
+
+	return AES256Cipher{aesgcm: aesgcm}, nil
+}
+
+var _ Cipher = AES256Cipher{}
+
+// Encrypt implementes the logic to encrypt plaintext
+func (a AES256Cipher) Encrypt(plaintext []byte) ([]byte, error) {
+
+	nonce := make([]byte, a.aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("generating nonce for encryption %v", err)
+	}
+
+	ciphertext := a.aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	// append nonce to ciphertext so decrypt can use it
+	ciphertext = append(ciphertext, nonce...)
+
+	return ciphertext, nil
+}
+
+// Decrypt implements the logic to decrypt ciphertext, it assumes
+// a nonce has been apended to ciphertext at encryption
+func (a AES256Cipher) Decrypt(ciphertext []byte) ([]byte, error) {
+	nonceIndex := len(ciphertext) - a.aesgcm.NonceSize()
+	cipher, nonce := ciphertext[:nonceIndex], ciphertext[nonceIndex:]
+
+	plaintext, err := a.aesgcm.Open(nil, nonce, cipher, nil)
+	if err != nil {
+		return nil, fmt.Errorf("decrypting ciphertext: %v", err)
+	}
+
+	return plaintext, nil
+}
---
+++ fleetshard/pkg/cipher/cipher_test.go
@@ -0,0 +1,52 @@
+package cipher
+
+import (
+	"crypto/rand"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func generateKey() ([]byte, error) {
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+// TestDifferentCipherForSamePlaintext tests the correct usage of nonce
+// to ensure encrypting the same plaintext twice does not yield the same cipher text
+func TestDifferentCipherForSamePlaintext(t *testing.T) {
+	plaintext := []byte("test plaintext")
+	key, err := generateKey()
+	require.NoError(t, err, "generating key")
+
+	aes, err := NewAES256Cipher(key)
+	require.NoError(t, err, "creating cipher")
+	cipher1, err := aes.Encrypt(plaintext)
+	require.NoError(t, err, "encrypting first plaintext")
+	cipher2, err := aes.Encrypt(plaintext)
+	require.NoError(t, err, "encrypting second plaintext")
+
+	require.NotEqual(t, cipher1, cipher2, "encrypting same text twice yields same result")
+}
+
+func TestEncryptDecryptMatch(t *testing.T) {
+	plaintext := []byte("test plaintext")
+	key, err := generateKey()
+	require.NoError(t, err, "generating key")
+
+	aes, err := NewAES256Cipher(key)
+	require.NoError(t, err, "creating cipher")
+
+	cipher, err := aes.Encrypt(plaintext)
+	require.NoError(t, err, "encyrpting plaintext")
+
+	decrypted, err := aes.Decrypt(cipher)
+	require.NoError(t, err, "decrypting ciphertext")
+
+	require.Equal(t, string(plaintext), string(decrypted), "decrypted string does not match plaintext")
+}
