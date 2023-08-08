176cda4e18359060078ccee1b88b43089a8a0d47
Ludovic Cleroux <ludydoo@gmail.com>
2023-08-08 10:18:10 +0200

    ROX-18497 GitOps configMap (#1167)

---
+++ internal/dinosaur/pkg/gitops/config.go
@@ -0,0 +1,93 @@
+// Package gitops contains the GitOps configuration.
+package gitops
+
+import (
+	"github.com/stackrox/rox/operator/apis/platform/v1alpha1"
+	"gopkg.in/yaml.v2"
+	field "k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// Config represents the gitops configuration
+type Config struct {
+	Centrals CentralsConfig `json:"centrals"`
+}
+
+// CentralsConfig represents the declarative configuration for Central instances defaults and overrides.
+type CentralsConfig struct {
+	// Default configuration for Central instances.
+	Default v1alpha1.Central `json:"default"`
+	// Overrides are the overrides for Central instances.
+	Overrides []CentralOverride `json:"overrides"`
+}
+
+// CentralOverride represents the configuration for a Central instance override. The override
+// will be applied on top of the default central instance configuration.
+// See https://github.com/stackrox/stackrox/blob/master/operator/apis/platform/v1alpha1/overlay_types.go
+type CentralOverride struct {
+	// InstanceIDs are the Central instance IDs for the override.
+	InstanceIDs []string `json:"instanceIds"`
+	// Patch is the patch for the override, which will be applied as a strategic merge patch.
+	Patch string `json:"patch"`
+}
+
+// ValidateConfig validates the GitOps configuration.
+func ValidateConfig(config Config) field.ErrorList {
+	var errs field.ErrorList
+	errs = append(errs, validateCentralsConfig(field.NewPath("centrals"), config.Centrals)...)
+	return errs
+}
+
+func validateCentralsConfig(path *field.Path, config CentralsConfig) field.ErrorList {
+	var errs field.ErrorList
+	errs = append(errs, validateCentralOverrides(path.Child("overrides"), config.Overrides)...)
+	return errs
+}
+
+func validateCentralOverrides(path *field.Path, config []CentralOverride) field.ErrorList {
+	var errs field.ErrorList
+	for i, override := range config {
+		errs = append(errs, validateCentralOverride(path.Index(i), override)...)
+	}
+	return errs
+}
+
+func validateCentralOverride(path *field.Path, config CentralOverride) field.ErrorList {
+	var errs field.ErrorList
+	errs = append(errs, validateInstanceIDs(path.Child("instanceIds"), config.InstanceIDs)...)
+	errs = append(errs, validatePatch(path.Child("patch"), config.Patch)...)
+	return errs
+}
+
+func validatePatch(path *field.Path, patch string) field.ErrorList {
+	var errs field.ErrorList
+	if len(patch) == 0 {
+		errs = append(errs, field.Required(path, "patch is required"))
+	}
+	// try to unmarshal the patch into a Central instance to validate it
+	if err := yaml.Unmarshal([]byte(patch), &v1alpha1.Central{}); err != nil {
+		errs = append(errs, field.Invalid(path, patch, "invalid patch: "+err.Error()))
+		return errs
+	}
+	return errs
+}
+
+func validateInstanceIDs(path *field.Path, instanceIDs []string) field.ErrorList {
+	var errs field.ErrorList
+	var seenInstanceIDs = make(map[string]struct{})
+	for i, instanceID := range instanceIDs {
+		if _, ok := seenInstanceIDs[instanceID]; ok {
+			errs = append(errs, field.Duplicate(path, instanceID))
+		}
+		seenInstanceIDs[instanceID] = struct{}{}
+		errs = append(errs, validateInstanceID(path.Index(i), instanceID)...)
+	}
+	return errs
+}
+
+func validateInstanceID(path *field.Path, instanceID string) field.ErrorList {
+	var errs field.ErrorList
+	if len(instanceID) == 0 {
+		errs = append(errs, field.Required(path, "instance ID is required"))
+	}
+	return errs
+}
