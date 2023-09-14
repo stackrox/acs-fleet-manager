76eadc69450f054d7305cd79ff357478c06a699e
Simon Bäumer <baeumer@pm.me>
2023-09-14 15:13:31 +0200

    Refactor build scripts to remove duplicated code (#1262)

--- build_push_fleet_manager.sh
+++ build_push_fleet_manager.sh
@@ -31,40 +31,10 @@
 # the built images can be pushed to quay.io.
 # =====================================================================================================================
 
-# The version should be a 7-char hash from git. This is what the deployment process in app-interface expects.
-VERSION=$(git rev-parse --short=7 HEAD)
-
 # Set image repository to default value if it is not passed via env
 IMAGE_REPOSITORY="${QUAY_IMAGE_REPOSITORY:-app-sre/acs-fleet-manager}"
 
-# Set the directory for docker configuration:
-DOCKER_CONFIG="${PWD}/.docker"
-
-# Log in to the image registry:
-if [ -z "${QUAY_USER}" ]; then
-  echo "The quay.io push user name hasn't been provided."
-  echo "Make sure to set the QUAY_USER environment variable."
-  exit 1
-fi
-if [ -z "${QUAY_TOKEN}" ]; then
-  echo "The quay.io push token hasn't been provided."
-  echo "Make sure to set the QUAY_TOKEN environment variable."
-  exit 1
-fi
-
-# Set up the docker config directory
-mkdir -p "${DOCKER_CONFIG}"
-
-BRANCH="main"
-if [[ -n "$GITHUB_REF" ]]; then
-  BRANCH="$(echo "$GITHUB_REF" | awk -F/ '{print $NF}')"
-  echo "GITHUB_REF is defined. Set image tag to $BRANCH."
-elif [[ -n "$GIT_BRANCH" ]]; then
-  BRANCH="$(echo "$GIT_BRANCH" | awk -F/ '{print $NF}')"
-  echo "GIT_BRANCH is defined. Set image tag to $BRANCH."
-else
-  echo "No git branch env var found. Set image tag to $BRANCH."
-fi
+source ./scripts/build_setup.sh
 
 # Push the image:
 echo "Quay.io user and token is set, will push images to $IMAGE_REPOSITORY"
