e04f49d3cf34f12cf5318cd8d9c1d09f0890ecc2
Aleksandr Kurlov <akurlov@redhat.com>
2023-10-04 17:09:49 +0200

    Fix gitops feature usage

--- scripts/install_operator_canary.sh
+++ scripts/install_operator_canary.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-kind load docker-image quay.io/rhacs-eng/stackrox-operator:4.2.0
-kind load docker-image quay.io/rhacs-eng/stackrox-operator:4.1.0
-
 export RHACS_TARGETED_OPERATOR_UPGRADES="true"
 export INSTALL_OLM="false"
 export INSTALL_OPERATOR="false"
