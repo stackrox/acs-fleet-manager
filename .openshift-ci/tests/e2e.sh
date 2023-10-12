38c80e74210c98029575ac6570484dc5e411f24f
Simon Baumer <baeumer@pm.me>
2023-10-12 09:13:43 +0200

    Enable feature flag gitops and targeted upgrades

--- .openshift-ci/tests/e2e.sh
+++ .openshift-ci/tests/e2e.sh
@@ -9,8 +9,10 @@ export GITROOT
 # shellcheck source=/dev/null
 source "${GITROOT}/dev/env/scripts/lib.sh"
 
-RUN_AUTH_E2E_DEFAULT="false"
-RUN_CENTRAL_E2E_DEFAULT="true"
+export RUN_AUTH_E2E="false"
+export RUN_CENTRAL_E2E="true"
+export RHACS_STANDALONE_MODE="true"
+export RHACS_GITOPS_ENABLED="true"
 
 if [[ "${OPENSHIFT_CI:-}" == "true" ]]; then
     # We are running in an OpenShift CI context, configure accordingly.
@@ -23,13 +25,14 @@ if [[ "${OPENSHIFT_CI:-}" == "true" ]]; then
         log "Got secret ${secret_name}"
         export "${secret_name}"="${secret_value}"
     done
+    export RHACS_TARGETED_OPERATOR_UPGRADES=true
     export STATIC_TOKEN="${FLEET_STATIC_TOKEN:-}"
     export STATIC_TOKEN_ADMIN="${FLEET_STATIC_TOKEN_ADMIN:-}"
     export CLUSTER_TYPE="openshift-ci"
     export GOARGS="-mod=mod" # For some reason we need this in the offical base images.
     export GINKGO_FLAGS="--no-color -v"
     # When running in OpenShift CI, ensure we also run the auth E2E tests.
-    RUN_AUTH_E2E_DEFAULT="true"
+    export RUN_AUTH_E2E_DEFAULT="true"
 fi
 
 init
@@ -39,8 +42,6 @@ if [[ "$SPAWN_LOGGER" == "true" && "$LOG_DIR" == "" ]]; then
     LOG_DIR=$(mktemp -d)
 fi
 export LOG_DIR
-export RUN_AUTH_E2E=${RUN_AUTH_E2E:-$RUN_AUTH_E2E_DEFAULT}
-export RUN_CENTRAL_E2E=${RUN_CENTRAL_E2E:-$RUN_CENTRAL_E2E_DEFAULT}
 
 log
 log "** Entrypoint for ACSCS E2E Tests **"
