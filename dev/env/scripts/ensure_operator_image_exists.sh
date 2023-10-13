4bcfeadf3bf1f20a183499a63681ee89743c6f21
Simon Baumer <baeumer@pm.me>
2023-10-13 10:10:20 +0200

    WIP

--- dev/env/scripts/ensure_operator_image_exists.sh
+++ dev/env/scripts/ensure_operator_image_exists.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 operator_images=$(yq e '.data."config.yaml"' dev/env/manifests/fleet-manager/04-gitops-config.yaml | yq -o json -M | jq -r '.rhacsOperators.operators[].image')
 
-for operator_image in "$operator_images"; do
+for operator_image in $operator_images; do
     docker pull "$operator_image"
     if [[ "$CLUSTER_TYPE" == "kind" ]]; then
             kind load docker-image "$operator_image"
