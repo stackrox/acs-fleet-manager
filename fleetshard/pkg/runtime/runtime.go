39cdb8d33d50047977e479de11ffa222d7de8e1a
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2023-10-20 10:16:26 +0200

    ROX-20268 Support running E2E tests on GKE (#1371)
    Implement support for deploying e2e test suite to GKE (support
    propagating tenant image pull secrets into tenant namespaces for pulling
    images from a private Quay repository).
    Version bump golangci-linter, because the old version was throwing
    errors.
    
    ---------
    
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>

--- fleetshard/pkg/runtime/runtime.go
+++ fleetshard/pkg/runtime/runtime.go
@@ -130,14 +130,15 @@ func (r *Runtime) Start() error {
 	routesAvailable := r.routesAvailable()
 
 	reconcilerOpts := centralReconciler.CentralReconcilerOptions{
-		UseRoutes:         routesAvailable,
-		WantsAuthProvider: r.config.CreateAuthProvider,
-		EgressProxyImage:  r.config.EgressProxyImage,
-		ManagedDBEnabled:  r.config.ManagedDB.Enabled,
-		Telemetry:         r.config.Telemetry,
-		ClusterName:       r.config.ClusterName,
-		Environment:       r.config.Environment,
-		AuditLogging:      r.config.AuditLogging,
+		UseRoutes:             routesAvailable,
+		WantsAuthProvider:     r.config.CreateAuthProvider,
+		EgressProxyImage:      r.config.EgressProxyImage,
+		ManagedDBEnabled:      r.config.ManagedDB.Enabled,
+		Telemetry:             r.config.Telemetry,
+		ClusterName:           r.config.ClusterName,
+		Environment:           r.config.Environment,
+		AuditLogging:          r.config.AuditLogging,
+		TenantImagePullSecret: r.config.TenantImagePullSecret, // pragma: allowlist secret
 	}
 
 	ticker := concurrency.NewRetryTicker(func(ctx context.Context) (timeToNextTick time.Duration, err error) {
