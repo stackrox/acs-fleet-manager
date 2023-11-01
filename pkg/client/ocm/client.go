9cc5607d0d0078ded525e33b03e0014d773469af
Yury Kovalev <ykovalev@redhat.com>
2023-11-01 13:40:14 +0100

    Further cleanup

--- pkg/client/ocm/client.go
+++ pkg/client/ocm/client.go
@@ -3,8 +3,9 @@ package ocm
 
 import (
 	"fmt"
-	"github.com/openshift-online/ocm-sdk-go/logging"
 	"net/http"
+
+	"github.com/openshift-online/ocm-sdk-go/logging"
 
 	"github.com/pkg/errors"
 	pkgerrors "github.com/pkg/errors"
@@ -41,10 +42,6 @@ type Client interface {
 	CreateAddon(clusterID string, addonID string) (*clustersmgmtv1.AddOnInstallation, error)
 	UpdateAddonParameters(clusterID string, addonID string, parameters []Parameter) (*clustersmgmtv1.AddOnInstallation, error)
 	GetClusterDNS(clusterID string) (string, error)
-	CreateSyncSet(clusterID string, syncset *clustersmgmtv1.Syncset) (*clustersmgmtv1.Syncset, error)
-	UpdateSyncSet(clusterID string, syncSetID string, syncset *clustersmgmtv1.Syncset) (*clustersmgmtv1.Syncset, error)
-	GetSyncSet(clusterID string, syncSetID string) (*clustersmgmtv1.Syncset, error)
-	DeleteSyncSet(clusterID string, syncsetID string) (int, error)
 	ScaleUpComputeNodes(clusterID string, increment int) (*clustersmgmtv1.Cluster, error)
 	ScaleDownComputeNodes(clusterID string, decrement int) (*clustersmgmtv1.Cluster, error)
 	SetComputeNodes(clusterID string, numNodes int) (*clustersmgmtv1.Cluster, error)
@@ -428,48 +425,6 @@ func (c *client) GetClusterDNS(clusterID string) (string, error) {
 	return clusterDNS, nil
 }
 
-// CreateSyncSet ...
-func (c client) CreateSyncSet(clusterID string, syncset *clustersmgmtv1.Syncset) (*clustersmgmtv1.Syncset, error) {
-	if c.connection == nil {
-		return nil, serviceErrors.InvalidOCMConnection()
-	}
-
-	clustersResource := c.connection.ClustersMgmt().V1().Clusters()
-	response, syncsetErr := clustersResource.Cluster(clusterID).
-		ExternalConfiguration().
-		Syncsets().
-		Add().
-		Body(syncset).
-		Send()
-	var err error
-	if syncsetErr != nil {
-		err = serviceErrors.NewErrorFromHTTPStatusCode(response.Status(), "ocm client failed to create syncset: %s", syncsetErr)
-	}
-	return response.Body(), err
-}
-
-// UpdateSyncSet ...
-func (c client) UpdateSyncSet(clusterID string, syncSetID string, syncset *clustersmgmtv1.Syncset) (*clustersmgmtv1.Syncset, error) {
-	if c.connection == nil {
-		return nil, serviceErrors.InvalidOCMConnection()
-	}
-
-	clustersResource := c.connection.ClustersMgmt().V1().Clusters()
-	response, syncsetErr := clustersResource.Cluster(clusterID).
-		ExternalConfiguration().
-		Syncsets().
-		Syncset(syncSetID).
-		Update().
-		Body(syncset).
-		Send()
-
-	var err error
-	if syncsetErr != nil {
-		err = serviceErrors.NewErrorFromHTTPStatusCode(response.Status(), "ocm client failed to update syncset '%s': %s", syncSetID, syncsetErr)
-	}
-	return response.Body(), err
-}
-
 // CreateIdentityProvider ...
 func (c client) CreateIdentityProvider(clusterID string, identityProvider *clustersmgmtv1.IdentityProvider) (*clustersmgmtv1.IdentityProvider, error) {
 	if c.connection == nil {
@@ -505,43 +460,6 @@ func (c client) GetIdentityProviderList(clusterID string) (*clustersmgmtv1.Ident
 		return nil, serviceErrors.NewErrorFromHTTPStatusCode(response.Status(), "ocm client failed to get list of identity providers, err: %s", getIDPErr.Error())
 	}
 	return response.Items(), nil
-}
-
-// GetSyncSet ...
-func (c client) GetSyncSet(clusterID string, syncSetID string) (*clustersmgmtv1.Syncset, error) {
-	if c.connection == nil {
-		return nil, serviceErrors.InvalidOCMConnection()
-	}
-
-	clustersResource := c.connection.ClustersMgmt().V1().Clusters()
-	response, syncsetErr := clustersResource.Cluster(clusterID).
-		ExternalConfiguration().
-		Syncsets().
-		Syncset(syncSetID).
-		Get().
-		Send()
-
-	var err error
-	if syncsetErr != nil {
-		err = serviceErrors.NewErrorFromHTTPStatusCode(response.Status(), "ocm client failed to get syncset '%s': %s", syncSetID, syncsetErr)
-	}
-	return response.Body(), err
-}
-
-// DeleteSyncSet Status returns the response status code.
-func (c client) DeleteSyncSet(clusterID string, syncsetID string) (int, error) {
-	if c.connection == nil {
-		return 0, serviceErrors.InvalidOCMConnection()
-	}
-
-	clustersResource := c.connection.ClustersMgmt().V1().Clusters()
-	response, syncsetErr := clustersResource.Cluster(clusterID).
-		ExternalConfiguration().
-		Syncsets().
-		Syncset(syncsetID).
-		Delete().
-		Send()
-	return response.Status(), syncsetErr
 }
 
 // ScaleUpComputeNodes scales up compute nodes by increment value
