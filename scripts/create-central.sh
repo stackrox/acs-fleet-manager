5113671c3820d76f4631c623aaf5706021651a47
Simon Baumer <baeumer@pm.me>
2023-11-03 11:00:50 +0100

    shellcheck pls be happy with my changes

--- scripts/create-central.sh
+++ scripts/create-central.sh
@@ -8,4 +8,4 @@ echo "Creating central tenant: $name"
 # shellcheck disable=SC1001
 curl -X POST -H "Authorization: Bearer $(ocm token)" -H "Content-Type: application/json" \
   http://127.0.0.1:8000/api/rhacs/v1/centrals\?async\=true \
-  -d '{"name": "'${name}'", "multi_az": true, "cloud_provider": "standalone", "region": "standalone"}'
+  -d '{"name": "'"${name}"'", "multi_az": true, "cloud_provider": "standalone", "region": "standalone"}'
