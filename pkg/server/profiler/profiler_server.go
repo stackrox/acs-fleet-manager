5446d65e8365700b9791dc197c9066475d6eed98
Simon Baumer <baeumer@pm.me>
2023-11-13 14:19:29 +0100

    fix test

--- pkg/server/profiler/profiler_server.go
+++ pkg/server/profiler/profiler_server.go
@@ -2,11 +2,14 @@
 package profiler
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/http"
 	"net/http/pprof"
 	"sync"
+
+	"github.com/golang/glog"
 
 	"github.com/gorilla/mux"
 	"github.com/stackrox/acs-fleet-manager/pkg/environments"
@@ -17,7 +20,9 @@ var _ server.Server = &PprofServer{}
 var _ environments.BootService = &PprofServer{}
 
 // PprofServer ...
-type PprofServer struct{}
+type PprofServer struct {
+	httpServer *http.Server
+}
 
 // Start ...
 func (p *PprofServer) Start() {
@@ -32,13 +37,31 @@ var (
 // SingletonPprofServer returns the PprofServer
 func SingletonPprofServer() *PprofServer {
 	oncePprofServer.Do(func() {
-		pprofServerInstance = &PprofServer{}
+		router := mux.NewRouter()
+		router.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
+		router.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+		router.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+		router.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+		router.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+		router.Handle("/debug/pprof/{cmd}", http.HandlerFunc(pprof.Index)) // special handling for Gorilla mux
+		httpServer := &http.Server{
+			Addr:    "localhost:6060",
+			Handler: router,
+		}
+
+		pprofServerInstance = &PprofServer{
+			httpServer: httpServer,
+		}
 	})
 	return pprofServerInstance
 }
 
 // Stop ...
 func (p *PprofServer) Stop() {
+	err := p.httpServer.Shutdown(context.Background())
+	if err != nil {
+		glog.Warningf("Unable to stop profiling server: %s", err)
+	}
 }
 
 // Listen ...
@@ -52,15 +75,7 @@ func (p *PprofServer) Serve(listener net.Listener) {
 
 // Run ...
 func (p *PprofServer) Run() {
-	router := mux.NewRouter()
-	router.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
-	router.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-	router.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-	router.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-	router.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
-	router.Handle("/debug/pprof/{cmd}", http.HandlerFunc(pprof.Index)) // special handling for Gorilla mux
-
-	err := http.ListenAndServe("localhost:6060", router)
+	err := p.httpServer.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("starting pprof server failed %s", err))
 	}
