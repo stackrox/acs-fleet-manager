198dd459e4ef736d701cca5c1e708dbc60cbacc0
Simon Bäumer <baeumer@pm.me>
2023-11-16 11:06:42 +0100

    Enable GitOps and TragetedUpgrade feature flags (#1472)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -5,8 +5,8 @@ var (
 	TargetedOperatorUpgrades = registerFeature("Upgrade Central instances targetedly via fleet-manager API", "RHACS_TARGETED_OPERATOR_UPGRADES", false)
 
 	// GitOpsCentrals enables the GitOps for Central instances
-	GitOpsCentrals = registerFeature("GitOps for Central instances", "RHACS_GITOPS_ENABLED", false)
+	GitOpsCentrals = registerFeature("GitOps for Central instances", "RHACS_GITOPS_ENABLED", true)
 
 	// PrintCentralUpdateDiff enables printing the diff of the central update
-	PrintCentralUpdateDiff = registerFeature("Print the diff of the central update", "RHACS_PRINT_CENTRAL_UPDATE_DIFF", false)
+	PrintCentralUpdateDiff = registerFeature("Print the diff of the central update", "RHACS_PRINT_CENTRAL_UPDATE_DIFF", true)
 )
