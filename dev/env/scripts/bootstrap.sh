b52b89827977e54385e7d5dda488a388b69cef9a
Simon Bäumer <baeumer@pm.me>
2023-11-21 14:06:29 +0100

    ROX-20148 - Remove operator installation from development scripts (#1471)

--- dev/env/scripts/bootstrap.sh
+++ dev/env/scripts/bootstrap.sh
@@ -15,7 +15,6 @@ cat <<EOF
 Image: ${FLEET_MANAGER_IMAGE}
 Namespace: ${ACSCS_NAMESPACE}
 Inheriting ImagePullSecrets for Quay.io: ${INHERIT_IMAGEPULLSECRETS}
-Installing RHACS Operator: ${INSTALL_OPERATOR}
 Operator Source: ${OPERATOR_SOURCE}
 Using OLM: ${INSTALL_OLM}
 Installing OpenShift Router: ${INSTALL_OPENSHIFT_ROUTER}
@@ -34,9 +33,6 @@ fi
 apply "${MANIFESTS_DIR}/shared"
 wait_for_default_service_account "$ACSCS_NAMESPACE"
 
-apply "${MANIFESTS_DIR}/rhacs-operator/00-namespace.yaml"
-wait_for_default_service_account "$STACKROX_OPERATOR_NAMESPACE"
-
 # pragma: allowlist nextline secret
 if [[ "$INHERIT_IMAGEPULLSECRETS" == "true" ]]; then
     create-imagepullsecrets
@@ -53,17 +49,10 @@ else
     log "Skipping installation of OpenShift Router"
 fi
 
-if [[ "$INSTALL_OPERATOR" == "true" ]]; then
-    log "Installing RHACS Operator"
-    install_operator.sh
-else
-    # We will be running without RHACS operator, but at least install our CRDs.
-    apply "${MANIFESTS_DIR}/crds"
-    apply "${MANIFESTS_DIR}/monitoring"
-fi
+apply "${MANIFESTS_DIR}/monitoring"
 
 if is_local_cluster "$CLUSTER_TYPE"; then
-    if [[ ("$INSTALL_OPERATOR" == "true" && "$OPERATOR_SOURCE" == "quay") || "$FLEET_MANAGER_IMAGE" =~ ^quay.io/ ]]; then
+    if [[  "$FLEET_MANAGER_IMAGE" =~ ^quay.io/ ]]; then
         if docker_logged_in "quay.io"; then
             log "Looks like we are already logged into Quay"
         else
