2c28637c43bd83f5e30fab9bfb02a0668b9358e5
Yury Kovalev <8366110+kovayur@users.noreply.github.com>
2023-12-20 16:27:34 +0100

    ROX-21070: Declare Data Plane Cluster configuration in gitops config (#1521)

--- internal/dinosaur/pkg/gitops/config.go
+++ internal/dinosaur/pkg/gitops/config.go
@@ -8,8 +8,9 @@ import (
 
 // Config represents the gitops configuration
 type Config struct {
-	Centrals       CentralsConfig           `json:"centrals"`
-	RHACSOperators operator.OperatorConfigs `json:"rhacsOperators"`
+	Centrals          CentralsConfig           `json:"centrals"`
+	RHACSOperators    operator.OperatorConfigs `json:"rhacsOperators"`
+	DataPlaneClusters []DataPlaneClusterConfig `json:"dataPlaneClusters"`
 }
 
 // CentralsConfig represents the declarative configuration for Central instances defaults and overrides.
@@ -28,11 +29,25 @@ type CentralOverride struct {
 	Patch string `json:"patch"`
 }
 
+// DataPlaneClusterConfig represents the configuration to be applied for a data plane cluster.
+type DataPlaneClusterConfig struct {
+	ClusterID string        `json:"clusterID"`
+	Addons    []AddonConfig `json:"addons"`
+}
+
+// AddonConfig represents the addon configuration to be installed on a cluster
+type AddonConfig struct {
+	ID         string            `json:"id"`
+	Version    string            `json:"version"`
+	Parameters map[string]string `json:"parameters"`
+}
+
 // ValidateConfig validates the GitOps configuration.
 func ValidateConfig(config Config) field.ErrorList {
 	var errs field.ErrorList
 	errs = append(errs, validateCentralsConfig(field.NewPath("centrals"), config.Centrals)...)
 	errs = append(errs, operator.Validate(field.NewPath("rhacsOperators"), config.RHACSOperators)...)
+	errs = append(errs, validateDataPlaneClusterConfigs(field.NewPath("dataPlaneClusters"), config.DataPlaneClusters)...)
 	return errs
 }
 
@@ -127,6 +142,49 @@ func validateInstanceID(path *field.Path, instanceID string) field.ErrorList {
 	var errs field.ErrorList
 	if len(instanceID) == 0 {
 		errs = append(errs, field.Required(path, "instance ID is required"))
+	}
+	return errs
+}
+
+func validateDataPlaneClusterConfigs(path *field.Path, clusters []DataPlaneClusterConfig) field.ErrorList {
+	var errs field.ErrorList
+	var seenCluster = make(map[string]struct{})
+	for i, cluster := range clusters {
+		errs = append(errs, validateClusterID(path.Index(i).Child("clusterID"), cluster.ClusterID)...)
+		if _, ok := seenCluster[cluster.ClusterID]; ok {
+			errs = append(errs, field.Duplicate(path, cluster))
+		}
+		seenCluster[cluster.ClusterID] = struct{}{}
+		errs = append(errs, validateAddons(path.Index(i).Child("addons"), cluster.Addons)...)
+	}
+	return errs
+}
+
+func validateClusterID(path *field.Path, clusterID string) field.ErrorList {
+	var errs field.ErrorList
+	if len(clusterID) == 0 {
+		errs = append(errs, field.Required(path, "clusterID is required"))
+	}
+	return errs
+}
+
+func validateAddons(path *field.Path, addons []AddonConfig) field.ErrorList {
+	var errs field.ErrorList
+	var seenAddon = make(map[string]struct{})
+	for i, addon := range addons {
+		errs = append(errs, validateAddonID(path.Index(i).Child("id"), addon.ID)...)
+		if _, ok := seenAddon[addon.ID]; ok {
+			errs = append(errs, field.Duplicate(path, addon))
+		}
+		seenAddon[addon.ID] = struct{}{}
+	}
+	return errs
+}
+
+func validateAddonID(path *field.Path, addonID string) field.ErrorList {
+	var errs field.ErrorList
+	if len(addonID) == 0 {
+		errs = append(errs, field.Required(path, "id is required"))
 	}
 	return errs
 }
