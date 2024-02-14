60a8d764eab418541c6332d98930e4943c741f98
Yury Kovalev <ykovalev@redhat.com>
2024-02-14 13:00:40 +0100

    Small refactoring

--- dev/env/scripts/ensure_operator_image_exists.sh
+++ dev/env/scripts/ensure_operator_image_exists.sh
@@ -6,10 +6,10 @@ operator_images="${operator_images} registry.redhat.io/openshift4/ose-kube-rbac-
 
 for operator_image in $operator_images; do
     if [[ -n "$(docker images -q "$operator_image")" ]]; then
-      echo "Found image $operator_image locally"
-      continue
+        echo "Found image $operator_image locally"
+    else
+        docker pull "$operator_image"
     fi
-    docker pull "$operator_image"
     if [[ "$CLUSTER_TYPE" == "kind" ]]; then
         kind load docker-image "$operator_image"
     fi
