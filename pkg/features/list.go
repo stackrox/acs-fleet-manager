ae432335b5b475b2371c02399895c6ea353ee092
Yury Kovalev <ykovalev@redhat.com>
2024-02-14 12:17:01 +0100

    Set minimal central resources and enable targeted updates by default

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -2,7 +2,7 @@ package features
 
 var (
 	// TargetedOperatorUpgrades enables the targeted operator upgrades
-	TargetedOperatorUpgrades = registerFeature("Upgrade Central instances targetedly via fleet-manager API", "RHACS_TARGETED_OPERATOR_UPGRADES", false)
+	TargetedOperatorUpgrades = registerFeature("Upgrade Central instances targetedly via fleet-manager API", "RHACS_TARGETED_OPERATOR_UPGRADES", true)
 
 	// GitOpsCentrals enables the GitOps for Central instances
 	GitOpsCentrals = registerFeature("GitOps for Central instances", "RHACS_GITOPS_ENABLED", true)
