ca9abe89e5ee84df0fb9eb4f39e360cdb4f114e1
ivan-degtiarenko <degtiarenko@stackrox.com>
2024-03-12 15:15:44 +0100

    Watch manual declarative config

--- fleetshard/pkg/k8s/secret.go
+++ fleetshard/pkg/k8s/secret.go
@@ -11,27 +11,29 @@ import (
 )
 
 const (
-	CentralTLSSecretName           = "central-tls"            // pragma: allowlist secret
-	centralDBPasswordSecretName    = "central-db-password"    // pragma: allowlist secret
-	centralEncryptionKeySecretName = "central-encryption-key" // pragma: allowlist secret
+	CentralTLSSecretName              = "central-tls"                              // pragma: allowlist secret
+	centralDBPasswordSecretName       = "central-db-password"                      // pragma: allowlist secret
+	centralEncryptionKeySecretName    = "central-encryption-key"                   // pragma: allowlist secret
+	manualDeclarativeConfigSecretName = "cloud-service-manual-declarative-configs" // pragma: allowlist secret
 )
 
-var defaultSecretsToWatch = []string{
-	CentralTLSSecretName,
-	centralEncryptionKeySecretName,
+var defaultSecretsToWatch = map[string]bool{
+	CentralTLSSecretName:              true,
+	centralEncryptionKeySecretName:    true,
+	manualDeclarativeConfigSecretName: false,
 }
 
 // SecretBackup is responsible for reading secrets to Backup for a tenant.
 type SecretBackup struct {
 	client         ctrlClient.Client
-	secretsToWatch []string
+	secretsToWatch map[string]bool
 }
 
 // NewSecretBackup creates a new instance of SecretService.
 func NewSecretBackup(client ctrlClient.Client, managedDB bool) *SecretBackup {
 	secretsToWatch := defaultSecretsToWatch // pragma: allowlist secret
 	if managedDB {
-		secretsToWatch = append(secretsToWatch, centralDBPasswordSecretName)
+		secretsToWatch[centralDBPasswordSecretName] = true
 	}
 
 	return &SecretBackup{client: client, secretsToWatch: secretsToWatch} // pragma: allowlist secret
@@ -39,8 +41,10 @@ func NewSecretBackup(client ctrlClient.Client, managedDB bool) *SecretBackup {
 
 // GetWatchedSecrets return a sorted list of secrets watched by this package
 func (s *SecretBackup) GetWatchedSecrets() []string {
-	secrets := make([]string, len(s.secretsToWatch))
-	copy(secrets, s.secretsToWatch)
+	secrets := make([]string, 0, len(s.secretsToWatch))
+	for secretName := range s.secretsToWatch {
+		secrets = append(secrets, secretName)
+	}
 	sort.Strings(secrets)
 	return secrets
 }
@@ -49,26 +53,31 @@ func (s *SecretBackup) GetWatchedSecrets() []string {
 // watched by SecretServices
 func (s *SecretBackup) CollectSecrets(ctx context.Context, namespace string) (map[string]*corev1.Secret, error) {
 	secrets := map[string]*corev1.Secret{}
-	for _, secretname := range s.secretsToWatch { // pragma: allowlist secret
-		secret, err := getSecret(ctx, s.client, secretname, namespace)
+	for secretName, required := range s.secretsToWatch { // pragma: allowlist secret
+		secret, found, err := getSecret(ctx, s.client, required, secretName, namespace)
 		if err != nil {
 			return nil, err
 		}
-		secrets[secretname] = secret // pragma: allowlist secret
+		if found {
+			secrets[secretName] = secret // pragma: allowlist secret
+		}
 	}
 
 	return secrets, nil
 }
 
-func getSecret(ctx context.Context, client ctrlClient.Client, secretname, namespace string) (*corev1.Secret, error) {
+func getSecret(ctx context.Context, client ctrlClient.Client, required bool, secretname, namespace string) (*corev1.Secret, bool, error) {
 	centralSecret := &corev1.Secret{}
 	err := client.Get(ctx, ctrlClient.ObjectKey{Namespace: namespace, Name: secretname}, centralSecret)
 	if err != nil {
 		if apiErrors.IsNotFound(err) {
-			return centralSecret, fmt.Errorf("%s secret not found", secretname)
+			if required {
+				return centralSecret, false, fmt.Errorf("%s secret not found", secretname)
+			}
+			return centralSecret, false, nil
 		}
-		return centralSecret, fmt.Errorf("getting secret %s/%s: %w", namespace, secretname, err)
+		return centralSecret, false, fmt.Errorf("getting secret %s/%s: %w", namespace, secretname, err)
 	}
 
-	return centralSecret, nil
+	return centralSecret, true, nil
 }
