c89eaf5a6e977273215631513325c03e97c7f9e2
Johannes Malsam <jmalsam@redhat.com>
2024-06-04 15:55:22 +0200

    generate auth config from service account token

--- emailsender/config/config.go
+++ emailsender/config/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/caarlos0/env/v6"
+	"github.com/golang-jwt/jwt/v4"
 	"gopkg.in/yaml.v2"
 
 	"github.com/pkg/errors"
@@ -12,16 +13,19 @@ import (
 	"github.com/stackrox/rox/pkg/errorhelpers"
 )
 
+const defaultSATokenFile = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+
 // Config contains this application's runtime configuration.
 type Config struct {
-	ClusterID      string `env:"CLUSTER_ID"`
-	ServerAddress  string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	EnableHTTPS    bool   `env:"ENABLE_HTTPS" envDefault:"false"`
-	HTTPSCertFile  string `env:"HTTPS_CERT_FILE" envDefault:""`
-	HTTPSKeyFile   string `env:"HTTPS_KEY_FILE" envDefault:""`
-	MetricsAddress string `env:"METRICS_ADDRESS" envDefault:":9090"`
-	AuthConfigFile string `env:"AUTH_CONFIG_FILE" envDefault:"config/emailsender-authz.yaml"`
-	AuthConfig     AuthConfig
+	ClusterID                    string `env:"CLUSTER_ID"`
+	ServerAddress                string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	EnableHTTPS                  bool   `env:"ENABLE_HTTPS" envDefault:"false"`
+	HTTPSCertFile                string `env:"HTTPS_CERT_FILE" envDefault:""`
+	HTTPSKeyFile                 string `env:"HTTPS_KEY_FILE" envDefault:""`
+	MetricsAddress               string `env:"METRICS_ADDRESS" envDefault:":9090"`
+	AuthConfigFile               string `env:"AUTH_CONFIG_FILE" envDefault:"config/emailsender-authz.yaml"`
+	AuthConfigFromServiceAccount bool   `env:"AUTH_CONFIG_FROM_SERVICE_ACCOUNT" envDefault:"true"`
+	AuthConfig                   AuthConfig
 }
 
 // GetConfig retrieves the current runtime configuration from the environment and returns it.
@@ -43,9 +47,16 @@ func GetConfig() (*Config, error) {
 		}
 	}
 
-	auth := &AuthConfig{file: c.AuthConfigFile}
-	if err := auth.ReadFile(); err != nil {
-		configErrors.AddError(err)
+	auth := &AuthConfig{file: c.AuthConfigFile, saTokenFile: defaultSATokenFile}
+	var authError error
+	if c.AuthConfigFromServiceAccount {
+		authError = auth.readFromSA()
+	} else {
+		authError = auth.readFile()
+	}
+
+	if authError != nil {
+		configErrors.AddError(authError)
 	}
 
 	c.AuthConfig = *auth
@@ -59,14 +70,15 @@ func GetConfig() (*Config, error) {
 // AuthConfig is the configuration for authn/authz for the emailsender
 type AuthConfig struct {
 	file             string
+	saTokenFile      string
 	JwksURLs         []string `yaml:"jwks_urls"`
 	AllowedIssuer    []string `yaml:"allowed_issuers"`
 	AllowedOrgIDs    []string `yaml:"allowed_org_ids"`
 	AllowedAudiences []string `yaml:"allowed_audiences"`
 }
 
-// ReadFile reads the config
-func (c *AuthConfig) ReadFile() error {
+// readFile reads the config
+func (c *AuthConfig) readFile() error {
 	fileContents, err := shared.ReadFile(c.file)
 	if err != nil {
 		return fmt.Errorf("failed to read emailsender authz config: %w", err)
@@ -76,6 +88,42 @@ func (c *AuthConfig) ReadFile() error {
 	if err != nil {
 		return fmt.Errorf("failed to unmarshal emailsender authz config: %w", err)
 	}
+
+	return nil
+}
+
+// readFromSA reads the file given as saTokenFile and uses the claims
+// to setup the AuthConfig. It is used for service account authentication of
+// tenants to the emailsender. tenants are running in the same cluster as
+// emailsender, thus their token issuer and keys must match.
+// It expects a jwks file to be available at URL %iss/keys.jsons
+func (c *AuthConfig) readFromSA() error {
+	tokenBytes, err := shared.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+	if err != nil {
+		return fmt.Errorf("failed to read service account token from file %w", err)
+	}
+
+	// we are parsing unverified here since injecting a service account token into the file above
+	// that has invalid information requires rights that would anyway allow to modify the
+	// configuration of this service including AuthConfig.
+	token, _, err := jwt.NewParser().ParseUnverified(string(tokenBytes), jwt.MapClaims{})
+	if err != nil {
+		return fmt.Errorf("failed to parse service account token: %w", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return fmt.Errorf("token does not include MapClaims")
+	}
+
+	issuer, ok := claims["iss"].(string)
+	if !ok {
+		return fmt.Errorf("issuer claim missing form token claims")
+	}
+
+	c.AllowedIssuer = []string{issuer}
+	c.AllowedAudiences = []string{issuer}
+	c.JwksURLs = []string{fmt.Sprintf("%s/keys.json", issuer)}
 
 	return nil
 }
