c16fb6e482ee6a55b95c4f32e396451fa3211df2
Yury Kovalev <ykovalev@redhat.com>
2024-06-07 22:35:27 +0200

    ROX-20474: Add TENANT_IMAGE_PULL_SECRET to the dp-terraform helm chart

--- dp-terraform/test/helm_template_test.go
+++ dp-terraform/test/helm_template_test.go
@@ -14,22 +14,11 @@ import (
 func TestHelmTemplate_FleetshardSyncDeployment_ServiceAccountTokenAuthType(t *testing.T) {
 	t.Parallel()
 
-	helmChartPath, err := filepath.Abs("../helm/rhacs-terraform")
-	releaseName := "rhacs-terraform"
-	require.NoError(t, err)
-
-	namespaceName := "rhacs"
-
-	options := &helm.Options{
-		SetValues: map[string]string{
-			"secured-cluster.enabled":          "false",
-			"fleetshardSync.managedDB.enabled": "false",
-			"fleetshardSync.authType":          "SERVICE_ACCOUNT_TOKEN",
-		},
-		KubectlOptions: k8s.NewKubectlOptions("", "", namespaceName),
-	}
-
-	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/fleetshard-sync.yaml"})
+	output := renderTemplate(t, map[string]string{
+		"secured-cluster.enabled":          "false",
+		"fleetshardSync.managedDB.enabled": "false",
+		"fleetshardSync.authType":          "SERVICE_ACCOUNT_TOKEN",
+	})
 
 	var deployment appsv1.Deployment
 	helm.UnmarshalK8SYaml(t, output, &deployment)
@@ -43,21 +32,110 @@ func TestHelmTemplate_FleetshardSyncDeployment_ServiceAccountTokenAuthType(t *te
 	require.Equal(t, "fleet-manager-token", volume.Name)
 
 	envVars := container.Env
-	require.Equal(t, "SERVICE_ACCOUNT_TOKEN", findEnvVar("AUTH_TYPE", envVars))
+	require.Equal(t, "SERVICE_ACCOUNT_TOKEN", findEnvVar("AUTH_TYPE", envVars).Value)
 	require.Empty(t, findEnvVar("RHSSO_SERVICE_ACCOUNT_CLIENT_ID", envVars))
 	require.Empty(t, findEnvVar("RHSSO_SERVICE_ACCOUNT_CLIENT_SECRET", envVars))
 	require.Empty(t, findEnvVar("RHSSO_REALM", envVars))
 	require.Empty(t, findEnvVar("RHSSO_ENDPOINT", envVars))
 
 	tokenFile := findEnvVar("FLEET_MANAGER_TOKEN_FILE", envVars)
-	require.NotEmpty(t, tokenFile)
+	require.NotEmpty(t, tokenFile.Value)
 }
 
-func findEnvVar(name string, envVars []corev1.EnvVar) string {
+func renderTemplate(t *testing.T, values map[string]string) string {
+	helmChartPath, err := filepath.Abs("../helm/rhacs-terraform")
+	releaseName := "rhacs-terraform"
+	require.NoError(t, err)
+
+	namespaceName := "rhacs"
+
+	options := &helm.Options{
+		SetValues:      values,
+		KubectlOptions: k8s.NewKubectlOptions("", "", namespaceName),
+	}
+
+	output := helm.RenderTemplate(t, options, helmChartPath, releaseName, []string{"templates/fleetshard-sync.yaml"})
+	return output
+}
+
+func findEnvVar(name string, envVars []corev1.EnvVar) *corev1.EnvVar {
 	for _, envVar := range envVars {
 		if envVar.Name == name {
-			return envVar.Value
+			return &envVar
 		}
 	}
-	return ""
+	return nil
+}
+
+func TestHelmTemplate_FleetshardSyncDeployment_Tenant(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name         string
+		ref          string
+		wantNoEnvVar bool
+		expectedRef  string
+		key          string
+		expectedKey  string
+	}{
+		{
+			name:         "should not add env var if ref value is not set",
+			wantNoEnvVar: true,
+		},
+		{
+			name:        "should add env var if ref value is set",
+			ref:         "stackrox",
+			expectedRef: "stackrox",
+		},
+		{
+			name:        "should set default key if ref value is set",
+			ref:         "stackrox",
+			expectedKey: ".dockerconfigjson",
+		},
+		{
+			name:        "should set key if ref and key is set",
+			ref:         "stackrox",
+			key:         "customkey",
+			expectedKey: "customkey",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			values := map[string]string{
+				"secured-cluster.enabled":          "false",
+				"fleetshardSync.managedDB.enabled": "false",
+			}
+
+			if tt.ref != "" {
+				values["fleetshardSync.tenantImagePullSecret.ref"] = tt.ref
+			}
+			if tt.key != "" {
+				values["fleetshardSync.tenantImagePullSecret.key"] = tt.key
+			}
+
+			output := renderTemplate(t, values)
+
+			var deployment appsv1.Deployment
+			helm.UnmarshalK8SYaml(t, output, &deployment)
+
+			container := deployment.Spec.Template.Spec.Containers[0]
+			require.Equal(t, "fleetshard-sync", container.Name)
+
+			envVars := container.Env
+
+			tenantImagePullSecret := findEnvVar("TENANT_IMAGE_PULL_SECRET", envVars)
+			if tt.wantNoEnvVar {
+				require.Empty(t, tenantImagePullSecret)
+				return
+			}
+			require.NotEmpty(t, tenantImagePullSecret)
+			if tt.expectedRef != "" {
+				require.Equal(t, tt.expectedRef, tenantImagePullSecret.ValueFrom.SecretKeyRef.Name)
+			}
+			if tt.expectedKey != "" {
+				require.Equal(t, tt.expectedKey, tenantImagePullSecret.ValueFrom.SecretKeyRef.Key)
+			}
+		})
+	}
 }
