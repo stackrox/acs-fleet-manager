9aaa8875347bf9d03a8c144439d507ecdf5025fb
Aleksandr Kurlov <akurlov@redhat.com>
2024-06-10 06:32:54 +0200

    Fix config

--- emailsender/config/config.go
+++ emailsender/config/config.go
@@ -37,8 +37,8 @@ type Config struct {
 	AuthConfigFile           string `env:"AUTH_CONFIG_FILE" envDefault:"config/emailsender-authz.yaml"`
 	AuthConfigFromKubernetes bool   `env:"AUTH_CONFIG_FROM_KUBERNETES" envDefault:"false"`
 	AuthConfig               AuthConfig
-  
-  DatabaseHost         string `env:"DATABASE_HOST" envDefault:"localhost"`
+	
+	DatabaseHost         string `env:"DATABASE_HOST" envDefault:"localhost"`
 	DatabasePort         int    `env:"DATABASE_PORT" envDefault:"5432"`
 	DatabaseName         string `env:"DATABASE_NAME" envDefault:"postgres"`
 	DatabaseUser         string `env:"DATABASE_USER" envDefault:"postgres"`
