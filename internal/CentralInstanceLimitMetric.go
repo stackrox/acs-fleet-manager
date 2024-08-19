79557862417877a8376fefbc324d998c821dce78
aaa5kameric <aaakameric5@gmail.com>
2024-08-19 12:09:19 +0200

    Updating Comments

--- internal/CentralInstanceLimitMetric.go
+++ internal/CentralInstanceLimitMetric.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/config"
+	"github.com/stackrox/acs-fleet-manager/internal/dinosaur/pkg/services"
+	apiErrors "github.com/stackrox/acs-fleet-manager/pkg/errors"
+	"log"
 	"net/http"
 )
 
@@ -32,13 +36,45 @@ type ClusterConfig struct {
 	clusterConfigMap map[string]ManualCluster
 }
 
+type ClustermanagerOptions struct {
+	DataplaneClusterConfig *config.DataplaneClusterConfig
+	ClusterService         services.ClusterService
+}
+
+type ClusterManager struct {
+	ClustermanagerOptions
+}
+type ResDinosaurInstanceCount struct {
+	Clusterid string
+	Count     int
+}
+type ClusterService interface {
+	FindDinosaurInstanceCount(clusterIDs []string) ([]ResDinosaurInstanceCount, *apiErrors.ServiceError)
+}
+
 // just gives me like the spots left in that cluster from the limit. Hopefully this is what we need for our metric
-func (conf *ClusterConfig) GettingLeftClusterInstance(clusterId string, currentFillings int) {
-	if cluster, exists := conf.clusterConfigMap[clusterId]; exists {
+func GettingLeftClusterInstance(clusterManager *ClusterManager) {
+	for _, cluster := range clusterManager.DataplaneClusterConfig.ClusterConfig.GetManualClusters() {
+		count, err := clusterManager.getClusterInstanceCount(clusterManager.ClusterID)
+		if err != nil {
+			log.Printf("Error getting count for cluster %s: %v", cluster.clusterID, err)
+			continue
+		}
 		limit := cluster.CentralInstanceLimit
-		LeftClusterSpots := float64(limit - currentFillings)
+		LeftClusterSpots := float64(limit - count)
 		CentralInstanceLimitMetric.WithLabelValues(cluster.ClusterID).Set(float64(LeftClusterSpots))
 	}
+}
+
+func (clusterManager *ClusterManager) getClusterInstanceCount(clusterID string) (int, error) {
+	counters, err := clusterManager.ClusterService.FindDinosaurInstanceCount([]string{clusterID})
+	if err != nil {
+		return 0, err
+	}
+	if len(counters) > 0 {
+		return counters[0].Count, nil
+	}
+	return 0, nil
 }
 
 func main() {
