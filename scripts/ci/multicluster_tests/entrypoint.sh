2096fa1afb6597f13891437adc9e26edc4346297
Johannes Malsam <jmalsam@redhat.com>
2024-12-03 14:37:01 +0100

    add initial script draft

---
+++ scripts/ci/multicluster_tests/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export CLUSTER_TYPE="infra-openshift"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$ROOT_DIR"
+
+source "$ROOT_DIR/scripts/ci/lib.sh"
+source "$ROOT_DIR/scripts/lib/log.sh"
+source "$ROOT_DIR/dev/env/scripts/lib.sh"
+
+bash "$SOURCE_DIR/run_multicluster_tests.sh"
+EXIT_CODE="$?"
+if [ "$EXIT_CODE" -ne "0" ]; then
+  log_failure
+fi
+
+stat /tmp/pids-port-forward > /dev/null 2>&1 && xargs kill < /tmp/pids-port-forward
+exit "$EXIT_CODE"
