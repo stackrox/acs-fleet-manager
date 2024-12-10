ef28c4b7ea123b96199f54435e659158cf891724
Johannes Malsam <60240743+johannes94@users.noreply.github.com>
2024-12-10 09:23:25 +0100

    ROX-27073: script for multi cluster test setup (#2119)
    * add env variable to only deploy dataplane components
    * add script to deploy to 2 clusters

---
+++ scripts/ci/multicluster_tests/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export CLUSTER_TYPE="infra-openshift"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$ROOT_DIR"
+
+source "$ROOT_DIR/scripts/ci/lib.sh"
+source "$ROOT_DIR/scripts/lib/log.sh"
+source "$ROOT_DIR/dev/env/scripts/lib.sh"
+
+bash "$SOURCE_DIR/deploy.sh"
+EXIT_CODE="$?"
+if [ "$EXIT_CODE" -ne "0" ]; then
+  echo "TODO(ROX-27073): add additional logging required here, once tests are actually executed"
+fi
+
+stat /tmp/pids-port-forward > /dev/null 2>&1 && xargs kill < /tmp/pids-port-forward
+exit "$EXIT_CODE"
