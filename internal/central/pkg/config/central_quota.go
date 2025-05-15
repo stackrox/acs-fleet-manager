f34ea762e896613505ed66fe085cbb400cc73412
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2025-05-15 14:17:28 +0200

    Apply suggestions from code review
    Co-authored-by: Johannes Malsam <60240743+johannes94@users.noreply.github.com>

--- internal/central/pkg/config/central_quota.go
+++ internal/central/pkg/config/central_quota.go
@@ -6,7 +6,7 @@ import "github.com/stackrox/acs-fleet-manager/pkg/api"
 type CentralQuotaConfig struct {
 	Type                   string `json:"type"`
 	AllowEvaluatorInstance bool   `json:"allow_evaluator_instance"`
-	// InternalOrganisationIds is a list of organisation IDs that shhould be ignored for quota checks
+	// InternalOrganisationIDs is a list of organisation IDs that should be ignored for quota checks
 	InternalOrganisationIDs []string `json:"internal_organisation_ids"`
 }
 
