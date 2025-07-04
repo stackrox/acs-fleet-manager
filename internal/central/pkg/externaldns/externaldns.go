93d0c0e69b400d50bff836b4f4a2c37f0a28aa07
Ludovic Cleroux <ludydoo@gmail.com>
2025-07-04 15:26:13 +0200

    pr comments

--- internal/central/pkg/externaldns/externaldns.go
+++ internal/central/pkg/externaldns/externaldns.go
@@ -2,6 +2,7 @@ package externaldns
 
 import "github.com/stackrox/acs-fleet-manager/internal/central/pkg/api/private"
 
+// IsEnabled checks if the external DNS feature is enabled for the given managed central.
 func IsEnabled(managedCentral private.ManagedCentral) bool {
 	isEnabled, ok := managedCentral.Spec.TenantResourcesValues["externalDnsEnabled"].(bool)
 	return ok && isEnabled
