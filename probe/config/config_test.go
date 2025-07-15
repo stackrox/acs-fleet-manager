0d53ff24ce4a15742183f73b0cd52ee23f5aae0d
Ludovic Cleroux <ludydoo@gmail.com>
2025-07-15 18:48:46 +0200

    fix env expand (#2394)

--- probe/config/config_test.go
+++ probe/config/config_test.go
@@ -13,12 +13,14 @@ func TestGetConfig_Success(t *testing.T) {
 	t.Setenv("AUTH_TYPE", "RHSSO")
 	t.Setenv("RHSSO_SERVICE_ACCOUNT_CLIENT_ID", "dummy")
 	t.Setenv("RHSSO_SERVICE_ACCOUNT_CLIENT_SECRET", "dummy")
+	t.Setenv("HOSTNAME", "hostname-dummy")
 
 	cfg, err := GetConfig()
 
 	require.NoError(t, err)
 	assert.Equal(t, cfg.FleetManagerEndpoint, "http://127.0.0.1:8888")
 	assert.Equal(t, cfg.ProbePollPeriod, 5*time.Second)
+	assert.Equal(t, cfg.ProbeName, "hostname-dummy")
 }
 
 func TestGetConfig_Failure(t *testing.T) {
