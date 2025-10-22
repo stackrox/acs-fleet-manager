a181eb956ef9c044c821641fcf1aff4de83e281d
Yury Kovalev <ykovalev@redhat.com>
2025-10-22 12:57:41 +0200

    Add additional prod RHSSO issuer to fix auth issues on integration

--- internal/central/pkg/environments/stage.go
+++ internal/central/pkg/environments/stage.go
@@ -12,10 +12,6 @@ func NewStageEnvLoader() environments.EnvLoader {
 		"max-allowed-instances":          "1",
 		"enable-central-external-domain": "true",
 		"cluster-compute-machine-type":   "m5.2xlarge",
-		"enable-additional-sso-issuers":  "true",
-		"additional-sso-issuers-file":    "config/additional-sso-issuers.yaml",
-		"jwks-file":                      "config/jwks-file-static.json",
-		"fleetshard-authz-config-file":   "config/fleetshard-authz-development.yaml",
 		"admin-authz-config-file":        "config/admin-authz-roles-dev.yaml",
 	}
 }
