52a6064703d2c73859e21936d0517a2549bcad03
Johannes Malsam <jmalsam@redhat.com>
2025-11-27 13:02:38 +0100

    better logs

--- scripts/ci/central_compatibility/entrypoint.sh
+++ scripts/ci/central_compatibility/entrypoint.sh
@@ -19,17 +19,20 @@ function log_failure() {
   log "Test failed with status: $EXIT_CODE"
   log "Starting to log cluster resources and container logs into $LOG_DIR directory"
 
-  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender > "$LOG_DIR/emailsender-deploy-describe.log"
-  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender > "$LOG_DIR/emailsender-pod-describe.log"
-  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender > "$LOG_DIR/emailsender-all-pods.log"
-
-  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender-db > "$LOG_DIR/emailsender-db-deploy-describe.log"
-  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender-db > "$LOG_DIR/emailsender-db-pod-describe.log"
-  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender-db > "$LOG_DIR/emailsender-db-all-pods.log"
+  kubectl describe deploy -n "$EMAILSENDER_NS" > "$LOG_DIR/emailsender-ns-deploy-describe.log"
+  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender > "$LOG_DIR/emailsender-ns-describe.log"
+  for pod in $(kubectl get pods -n "$EMAILSENDER_NS" -o name); do
+    pod_name=$(basename "$pod")
+    kubectl logs -n "$EMAILSENDER_NS" "$pod_name" --all-containers --prefix > "$LOG_DIR/${pod_name}.log"
+  done
 
   kubectl describe deploy -n "$CENTRAL_NS" > "$LOG_DIR/central-ns-deploy-describe.log"
   kubectl describe pods -n "$CENTRAL_NS" > "$LOG_DIR/central-ns-pod-describe.log"
-  kubectl logs -n "$CENTRAL_NS" --prefix --all-containers -l "app.kubernetes.io/name=stackrox" > "$LOG_DIR/central-ns-all-pods.log"
+  # Log each central namespace pod separately
+  for pod in $(kubectl get pods -n "$CENTRAL_NS" -l "app.kubernetes.io/name=stackrox" -o name); do
+    pod_name=$(basename "$pod")
+    kubectl logs -n "$CENTRAL_NS" "$pod_name" --all-containers --prefix > "$LOG_DIR/${pod_name}.log"
+  done
 }
 
 bash "$SOURCE_DIR/run_compatibility_test.sh"
