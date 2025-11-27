b7e62dfad199bea7de71fd2916e0ffa0356f9df0
Johannes Malsam <jmalsam@redhat.com>
2025-11-27 11:56:16 +0100

    fix log dir

--- scripts/ci/central_compatibility/entrypoint.sh
+++ scripts/ci/central_compatibility/entrypoint.sh
@@ -11,26 +11,25 @@ source "$ROOT_DIR/dev/env/scripts/lib.sh"
 
 export EMAILSENDER_NS="rhacs"
 export CENTRAL_NS="rhacs-tenant"
-mkdir logs
+
+LOG_DIR="$GITHUB_WORKSPACE/logs"
+mkdir "$LOG_DIR"
+
 function log_failure() {
   log "Test failed with status: $EXIT_CODE"
-  log "Starting to log cluster resources and container logs..."
+  log "Starting to log cluster resources and container logs into $LOG_DIR directory"
 
-  log "***** START EMAILSENDER RESOURCES *****"
-  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender > logs/emailsender-deploy-describe.log
-  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender > logs/emailsender-pod-describe.log
-  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender > logs/emailsender-all-pods.log
+  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender > "$LOG_DIR/emailsender-deploy-describe.log"
+  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender > "$LOG_DIR/emailsender-pod-describe.log"
+  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender > "$LOG_DIR/emailsender-all-pods.log"
 
-  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender-db > logs/emailsender-db-deploy-describe.log
-  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender-db > logs/emailsender-db-pod-describe.log
-  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender-db > logs/emailsender-db-all-pods.log
-  log "***** END EMAILSENDER RESOURCES *****"
+  kubectl describe deploy -n "$EMAILSENDER_NS" emailsender-db > "$LOG_DIR/emailsender-db-deploy-describe.log"
+  kubectl describe pods -n "$EMAILSENDER_NS" -l app=emailsender-db > "$LOG_DIR/emailsender-db-pod-describe.log"
+  kubectl logs -n "$EMAILSENDER_NS" --prefix --all-containers -l app=emailsender-db > "$LOG_DIR/emailsender-db-all-pods.log"
 
-  log "***** START STACKROX KUBERNETES RESOURCES *****"
-  kubectl describe deploy -n "$CENTRAL_NS" > logs/central-ns-deploy-describe.log
-  kubectl describe pods -n "$CENTRAL_NS" > logs/central-ns-pod-describe.log
-  kubectl logs -n "$CENTRAL_NS" --prefix --all-containers -l "app.kubernetes.io/name=stackrox" > logs/central-ns-all-pods.log
-  log "***** END STACKROX KUBERNETES RESOURCES *****"
+  kubectl describe deploy -n "$CENTRAL_NS" > "$LOG_DIR/central-ns-deploy-describe.log"
+  kubectl describe pods -n "$CENTRAL_NS" > "$LOG_DIR/central-ns-pod-describe.log"
+  kubectl logs -n "$CENTRAL_NS" --prefix --all-containers -l "app.kubernetes.io/name=stackrox" > "$LOG_DIR/central-ns-all-pods.log"
 }
 
 bash "$SOURCE_DIR/run_compatibility_test.sh"
